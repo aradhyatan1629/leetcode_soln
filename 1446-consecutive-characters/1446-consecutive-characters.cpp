399881f7db26e57a74e3214933f1e127be985ddd
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2022-07-27 23:10:42 +0530

    Time: 11 ms (11.81%), Space: 6.8 MB (26.94%) - LeetHub

--- 1446-consecutive-characters/1446-consecutive-characters.cpp
+++ 1446-consecutive-characters/1446-consecutive-characters.cpp
@@ -1,25 +1,16 @@
 class Solution {
 public:
     int maxPower(string s) {
-        int i=0,j=0,length=1;
-        int ans=INT_MIN;
-        for(i=0;i<s.size();i++)
+        int i=0,length=1,ans=1;
+        for(i=1;i<s.size();i++)
         {
-            length=1;
-            for(j=i+1;j<s.size();j++)
+            if(s[i]==s[i-1])
             {
-                if(s[i]==s[j])
-                {
-                    length++;
-                }
-                else
-                {
-                    break;
-                }
+                ans = max(ans,++length);
             }
-            if(length>ans)
+            else
             {
-                ans=length;
+                length=1;
             }
         }
         return ans;
