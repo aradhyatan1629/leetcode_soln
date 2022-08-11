71764d76257a8f0af68daaa8c69545c94e6f7924
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2022-08-11 14:43:47 +0530

    Time: 10 ms (79.94%), Space: 10.8 MB (60.49%) - LeetHub

--- 1629-slowest-key/1629-slowest-key.cpp
+++ 1629-slowest-key/1629-slowest-key.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
     char slowestKey(vector<int>& r, string key) {
-        map<char,int> m;
-        m[key[0]] = r[0];
-        int mx = INT_MIN;
-        char ans;
+        int mx = r[0];
+        char prev = key[0];
+        int index;
         for(int i=1;i<key.size();i++)
         {
-            int press = r[i] - r[i-1];
-            if(press>=m[key[i]])
+            int press = r[i]-r[i-1];
+            if(press>mx)
             {
-                m[key[i]] = press;
+                mx = press;
+                prev=key[i];
+            }
+            if(press==mx and key[i]>=prev)
+            {
+                mx=press;
+                prev=key[i];
             }
         }
-        for(auto x:m)
-        {
-            if(x.second>=mx)
-            {
-                mx=x.second;
-                ans=x.first;
-            }
-        }
-        return ans;
+        return prev;
     }
 };
