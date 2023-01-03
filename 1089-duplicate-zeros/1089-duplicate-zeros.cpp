84ff3d465671b64cd73406bc5637b565db7220a0
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2023-01-03 23:53:44 +0530

    Time: 179 ms (13.99%), Space: 9.6 MB (93.74%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.cpp
+++ 1089-duplicate-zeros/1089-duplicate-zeros.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
+    void shift(vector<int> &arr,int n,int ind)
+    {
+        for(int i=n-2;i>ind;i--)
+        {
+            arr[i+1]=arr[i];
+        }
+    }
     void duplicateZeros(vector<int>& arr) {
-        int n=arr.size();
-        for(int i=0;i<n-1;i++)
+        for(int i=0;i<arr.size()-1;i++)
         {
             if(arr[i]==0)
             {
-                for(int j=n-2;j>i;j--)
-                {
-                    arr[j+1]=arr[j];
-                }
+                shift(arr,arr.size(),i);
                 arr[i+1]=0;
                 i++;
             }
