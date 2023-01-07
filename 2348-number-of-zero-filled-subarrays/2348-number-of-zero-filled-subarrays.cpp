487e5e9d67f83d5d332dce313a3bb6039fdc9904
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2023-01-07 15:33:59 +0530

    Time: 186 ms (73.90%), Space: 113.9 MB (5.33%) - LeetHub

--- 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.cpp
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     long long zeroFilledSubarray(vector<int>& nums) {
-        long long int ans=0;
-        long long int cnt=0;
+        int cnt=0;
+        vector<long long> v;
         for(int i=0;i<nums.size();i++)
         {
             if(nums[i]==0)
@@ -11,14 +11,45 @@ public:
             }
             else
             {
-                ans+=((cnt*(cnt+1))/2);
+                v.push_back(cnt);
                 cnt=0;
             }
         }
         if(cnt!=0)
         {
-            ans+=((cnt*(cnt+1))/2);
+            v.push_back(cnt);
+        }
+        long long int ans=0;
+        for(auto x:v)
+        {
+            if(x%2==0)
+            {
+                long long int t = x/2;
+                ans+=(x*t)+t;
+            }
+            else
+            {
+                ans+=(x*((x/2)+1));
+            }
         }
         return ans;
     }
 };
+
+// 0 0 0 0 0 0
+/*
+1 - 1
+2 - 3
+3 - 6
+4 - 10
+5 - 15
+6 - 21
+*/
+
+/*
+1*1
+3*2
+5*3
+7*4
+
+*/
