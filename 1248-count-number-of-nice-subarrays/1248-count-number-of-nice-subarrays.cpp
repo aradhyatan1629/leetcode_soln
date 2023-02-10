9569bf0b9812a9c7e3d45f1054adc7d308488c90
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2023-02-10 12:44:39 +0530

    Time: 314 ms (24.28%), Space: 102.4 MB (11.38%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -2,40 +2,35 @@ class Solution {
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
         unordered_map<int,int> m;
-        vector<int> v;
         int cnt=0;
-        for(auto x:nums)
+        for(int i=0;i<nums.size();i++)
         {
-            if(x%2==0)
+            if(nums[i]%2==0)
             {
-                v.push_back(0);
+                nums[i]=0;
             }
             else
             {
-                v.push_back(1);
+                nums[i]=1;
             }
         }
-        vector<int> pre(v.size(),0);
-        pre[0]=v[0];
-        for(int i=1;i<v.size();i++)
+        for(int i=1;i<nums.size();i++)
         {
-            pre[i]=pre[i-1]+v[i];
+            nums[i]=nums[i-1]+nums[i];
         }
-        for(int i=0;i<pre.size();i++)
+        for(int i=0;i<nums.size();i++)
         {
-            m[pre[i]]++;
-            if(pre[i]==k)
+            m[nums[i]]++;
+            if(nums[i]==k)
             {
                 cnt++;
             }
-            if(m[pre[i]-k]>0)
+            if(m[nums[i]-k]>0)
             {
-                cnt+=m[pre[i]-k];
+                cnt+=m[nums[i]-k];
             }
         }
         return cnt;
     }
 };
 
-// 0 0 0 1 0 0 1 0 0 0
-// 0 0 0 1 1 1 2 2 2 2
