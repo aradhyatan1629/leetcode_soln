c0243b04953f89f260f571953bf8745979ceb7fc
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2023-06-20 13:43:00 +0530

    Time: 258 ms (58.32%), Space: 145.6 MB (17.59%) - LeetHub

--- 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.cpp
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.cpp
@@ -1,43 +1,40 @@
 class Solution {
 public:
     vector<int> getAverages(vector<int>& nums, int k) {
-        if(k==0)
-        {
-            return nums;
-        }
-        int n=nums.size();
-        vector<long long int> pre(n,0);
+        int n = nums.size(),sz=0;
+        vector<long long> pre(n,0);
         vector<int> ans;
-        pre[0]=nums[0];
+        pre[0] = nums[0];
+        int j=0;
         for(int i=1;i<nums.size();i++)
         {
-            pre[i]=pre[i-1] + nums[i];
+            pre[i] =  pre[i-1] + nums[i];
         }
         for(int i=0;i<pre.size();i++)
         {
-            if(i-k<0 or i+k>=n)
+            if(i-k>=0 and i+k<pre.size())
             {
-                ans.push_back(-1);
-            }
-            else if(i-k==0)
-            {
-                long long int s = pre[i+k];
-                long long int t = (s/((k*2)+1));
-                ans.push_back(t);
+                if(i-k==0)
+                {
+                    sz = i+k+1;
+                    ans.push_back(pre[i+k]/sz);
+                }
+                else
+                {
+                    ans.push_back((pre[i+k]-pre[j])/sz);
+                    j++;
+                }
             }
             else
             {
-                long long int s = pre[i+k] - pre[i-k-1];
-                long long int t = (s/((k*2)+1));
-                ans.push_back(t);
+                ans.push_back(-1);
             }
         }
         return ans;
     }
 };
 
-// 0 1 2 3 4 5 6 7 8
-// 7 4 3 9 1 8 5 2 6
 
-// 7 11 14 23 24 32 37 39 45
-// 0  1  2  3  4  5  6  7  8
+/*
+7 11 14 23 24 32 37 39 45
+*/
