53ea78c90bc6aae441b78bd5661f700204671544
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2023-07-06 00:59:42 +0530

    Time: 44 ms (67.28%), Space: 36.5 MB (65.98%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -1,65 +1,38 @@
 class Solution {
 public:
     int longestSubarray(vector<int>& nums) {
-        int l=0,h=0;
-        vector<int> v;
-        int sum=accumulate(nums.begin(),nums.end(),0);
-        if(sum==nums.size())
+        int n=nums.size();
+        int i=0,j=0;
+        int count0=0,ans=0;
+        while(j<n)
         {
-            return nums.size()-1;
-        }
-        for(int i=0;i<nums.size();i++)
-        {
-            if(nums[i]==0 and i==0)
+            if(count0==0)
             {
-                l=0;
-                int j=1;
-                while(nums[j]==1)
+                if(nums[j]==0)
                 {
-                    h++;
+                    count0++;
+                }
+                j++;
+            }
+            else
+            {
+                if(nums[j]==1)
+                {
                     j++;
                 }
-                v.push_back(l+h);
-                l=0,h=0;
-            }
-            else if(nums[i]==0 and i==nums.size()-1)
-            {
-                h=0;
-                int j=i-1;
-                while(nums[j]==1)
+                else
                 {
-                    h++;
-                    j--;
-                }
-                v.push_back(l+h);
-                l=0,h=0;
-            }
-            else if(nums[i]==0 and (i!=0 and i!=nums.size()-1))
-            {
-                int j=i-1;
-                int k=i+1;
-                while(nums[j]==1)
-                {
-                    l++;
-                    j--;
-                    if(j<0)
+                    ans=max(ans,j-i-1);
+                    if(nums[i]==0)
                     {
-                        break;
+                        count0--;
                     }
+                    i++;
                 }
-                while(nums[k]==1)
-                {
-                    h++;
-                    k++;
-                    if(k==nums.size())
-                    {
-                        break;
-                    }
-                }
-                v.push_back(l+h);
-                l=0,h=0;
             }
         }
-        return *max_element(v.begin(),v.end());
+        ans = max(ans,j-i-1);
+        return ans;
     }
 };
+
