313f4963dd16069727b7da4718f7b872acb12887
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2023-07-10 00:59:45 +0530

    Added solution - LeetHub

--- Number Of Enclaves - GFG/number-of-enclaves.cpp
+++ Number Of Enclaves - GFG/number-of-enclaves.cpp
@@ -11,46 +11,42 @@ using namespace std;
 class Solution {
   public:
     int numberOfEnclaves(vector<vector<int>> &grid) {
-        queue<pair<int,int>> q;
         int n = grid.size();
         int m = grid[0].size();
-        int vis[n][m] = {0};
+        vector<vector<int>> vis(n,vector<int>(m,0));
+        queue<pair<int,int>> q;
         
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                if(i==0 or j==0 or i==n-1 or j==m-1)
+                if((i==0 or i==n-1 or j==0 or j==m-1) and grid[i][j]==1)
                 {
-                    if(grid[i][j]==1)
-                    {
-                        q.push({i,j});
-                        vis[i][j] = 1;
-                    }
+                    vis[i][j]=1;
+                    q.push({i,j});
                 }
             }
         }
         int delRow[] = {-1,0,+1,0};
         int delCol[] = {0,+1,0,-1};
-        
         while(!q.empty())
         {
             int row = q.front().first;
             int col = q.front().second;
             q.pop();
-            
             for(int i=0;i<4;i++)
             {
                 int nrow = row + delRow[i];
                 int ncol = col + delCol[i];
-                if(nrow>=0 and nrow<n and ncol>=0 and ncol<m and vis[nrow][ncol]==0 and grid[nrow][ncol]==1)
+                if(nrow>=0 and nrow<n and ncol>=0 and ncol<m and !vis[nrow][ncol] and grid[nrow][ncol]==1)
                 {
                     vis[nrow][ncol] = 1;
                     q.push({nrow,ncol});
                 }
             }
         }
-        int cnt = 0;
+        
+        int cnt=0;
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
