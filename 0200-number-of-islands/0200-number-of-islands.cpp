a5675b2dd34c2de57dd82dfa08da6558898dadd0
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2023-07-09 20:46:30 +0530

    Time: 46 ms (39.49%), Space: 13.3 MB (46.30%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,36 +1,27 @@
 class Solution {
 public:
-    void bfs(int row,int col,vector<vector<char>> &grid,vector<vector<int>> &vis,int delRow[],int delCol[])
+    void dfs(int row,int col,vector<vector<char>> &grid,vector<vector<int>> &vis,int delRow[],int delCol[])
     {
         vis[row][col] = 1;
-        queue<pair<int,int>> q;
-        q.push({row,col});
-        int m = grid.size();
-        int n = grid[0].size();
+        int m=grid.size();
+        int n=grid[0].size();
         
-        while(!q.empty())
+        for(int i=0;i<4;i++)
         {
-            int row = q.front().first;
-            int col = q.front().second;
-            q.pop();
-            for(int i=0;i<4;i++)
+            int nrow = row + delRow[i];
+            int ncol = col + delCol[i];
+            if(nrow>=0 and nrow<m and ncol>=0 and ncol<n and grid[nrow][ncol]=='1' and vis[nrow][ncol]==0)
             {
-                int nrow = row + delRow[i];
-                int ncol = col + delCol[i];
-                if(nrow>=0 and nrow<m and ncol>=0 and ncol<n and vis[nrow][ncol]==0 and grid[nrow][ncol]=='1')
-                {
-                    q.push({nrow,ncol});
-                    vis[nrow][ncol] = 1;
-                }
+                dfs(nrow,ncol,grid,vis,delRow,delCol);
             }
         }
     }
     
     int numIslands(vector<vector<char>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
+        int m=grid.size();
+        int n=grid[0].size();
         vector<vector<int>> vis(m,vector<int>(n,0));
-        int cnt=0;
+        int cnt = 0;
         int delRow[] = {-1,0,+1,0};
         int delCol[] = {0,+1,0,-1};
         
@@ -41,7 +32,7 @@ public:
                 if(grid[i][j]=='1' and vis[i][j]==0)
                 {
                     cnt++;
-                    bfs(i,j,grid,vis,delRow,delCol);
+                    dfs(i,j,grid,vis,delRow,delCol);
                 }
             }
         }
