e22d838e395d9b3f2679b2bf843809ee7664b752
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2023-07-15 13:57:06 +0530

    Time: 121 ms (69.19%), Space: 67.8 MB (35.46%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    
+    //Kadane's Akgorithm
+    int maxSubArray(vector<int>& nums) {
+        int mx = INT_MIN;
+        int sum=0;
+        
+        for(int i=0;i<nums.size();i++)
+        {
+            sum+=nums[i];
+            if(sum > mx)
+            {
+                mx = sum;
+            }
+            if(sum<0)
+            {
+                sum=0;
+            }
+        }
+        return mx;
+    }
+};
