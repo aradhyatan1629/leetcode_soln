44154ba95a1724ab704788c2a1652d76763e9cff
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2023-08-16 16:09:09 +0530

    Time: 331 ms (36.31%), Space: 155.3 MB (19.09%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        vector<int> v;
-        int i=0,j=0,n=nums.size();
-        priority_queue<int> pq;
+        vector<int> ans;
         unordered_map<int,int> m;
+        priority_queue<int> pq;
+        int i=0,j=0,n=nums.size();
         
         while(j<n)
         {
@@ -18,16 +18,15 @@ public:
             {
                 if(m[pq.top()]!=0)
                 {
-                    // cout<<pq.top()<<" "<<m[pq.top()]<<endl;
-                    v.push_back(pq.top());
+                    ans.push_back(pq.top());
                 }
                 else
                 {
-                   while(pq.size()>0 and m[pq.top()]==0)
-                   {
-                       pq.pop();
-                   }
-                   v.push_back(pq.top());
+                    while(pq.size()>0 and m[pq.top()]==0)
+                    {
+                        pq.pop();
+                    }
+                    ans.push_back(pq.top());
                 }
                 if(m[nums[i]]>=1)
                 {
@@ -37,10 +36,6 @@ public:
                 j++;
             }
         }
-        return v;
+        return ans;
     }
 };
-
-
-
-
