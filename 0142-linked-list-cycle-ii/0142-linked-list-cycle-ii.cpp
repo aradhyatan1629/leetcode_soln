db23d652b59c42364a76c9346257970a50a2c3a2
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2023-08-19 17:37:15 +0530

    Time: 9 ms (47.23%), Space: 7.8 MB (27.20%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -8,21 +8,32 @@
  */
 class Solution {
 public:
+    //O(n) O(1)
+    //1) Find collision point
+    //2) Find the starting point of loop
+    
     ListNode *detectCycle(ListNode *head) {
-        unordered_map<ListNode*,int> m;
-        ListNode *temp = head;
-        
-        while(temp)
+        if(head==NULL)
         {
-            if(m[temp]==0)
+            return NULL;
+        }
+        ListNode *slow=head;
+        ListNode *fast=head;
+        ListNode *entry=head;
+        
+        while(fast->next and fast->next->next) //finding the collision point
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast)
             {
-                m[temp]++;
+                while(slow!=entry)    //finding the starting point of the loop
+                {
+                    slow=slow->next;
+                    entry=entry->next;
+                }
+                return entry;
             }
-            else
-            {
-                return temp;
-            }
-            temp=temp->next;
         }
         return NULL;
     }
