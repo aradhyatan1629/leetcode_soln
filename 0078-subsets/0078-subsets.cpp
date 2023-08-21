71fdf45a2fcd222b88c1842ea57f396f746a9cc4
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2023-08-21 21:19:56 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (77.47%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
-    vector<vector<int>> ans;
-    void f(int ind,vector<int> &subs,vector<int> &nums,int n)
+    
+    void f(int ind,vector<int> &subs,vector<int> &nums,vector<vector<int>> &ans,int n)
     {
         if(ind>=n)
         {
             ans.push_back(subs);
             return;
         }
-        else
-        {
-            subs.push_back(nums[ind]);
-            f(ind+1,subs,nums,n);
-            subs.pop_back();
-            f(ind+1,subs,nums,n);
-        }
+        subs.push_back(nums[ind]); //adding the element in our subsequence array
+        f(ind+1,subs,nums,ans,n);  //take 
+        subs.pop_back();           //bringing the array to the state it was in
+        f(ind+1,subs,nums,ans,n);  //not take
     }
+    
     vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
         vector<int> subs;
-        int n=nums.size();
-        f(0,subs,nums,n);
+        f(0,subs,nums,ans,nums.size());
         return ans;
     }
 };
