d3176fa6f70107a0a1bfb8b9fa7df4e267711893
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2023-08-28 01:09:24 +0530

    Time: 4 ms (90.93%), Space: 14.2 MB (27.16%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,22 +1,32 @@
 class Solution {
 public:
-    
-    void dfs(vector<vector<int>> &image,int i,int j,int initialColor,int newColor)
+    void dfs(int row,int col,vector<vector<int>> &image,vector<vector<int>> &vis,int delRow[],int delCol[],int color,int icolor)
     {
-        if(i<0 or i>=image.size() or j<0 or j>=image[0].size() or image[i][j]==newColor or                   image[i][j]!=initialColor)
+        int m=image.size();
+        int n=image[0].size();
+        vis[row][col] = 1;
+        image[row][col] = color;
+        for(int i=0;i<4;i++)
         {
-            return;
+            int nrow = row + delRow[i];
+            int ncol = col + delCol[i];
+            if(nrow>=0 and nrow<m and ncol>=0 and ncol<n and image[nrow][ncol]==icolor and !vis[nrow][ncol])
+            {
+                dfs(nrow,ncol,image,vis,delRow,delCol,color,icolor);
+            }
         }
-        image[i][j] = newColor;
-        dfs(image,i-1,j,initialColor,newColor);
-        dfs(image,i+1,j,initialColor,newColor);
-        dfs(image,i,j-1,initialColor,newColor);
-        dfs(image,i,j+1,initialColor,newColor);
     }
     
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int initialColor = image[sr][sc];
-        dfs(image,sr,sc,initialColor,color);
+        int m = image.size();
+        int n = image[0].size();
+        vector<vector<int>> vis(m,vector<int>(n,0));
+        int delRow[] = {-1,0,+1,0};
+        int delCol[] = {0,+1,0,-1};
+        int icolor = image[sr][sc];
+        
+        dfs(sr,sc,image,vis,delRow,delCol,color,icolor);
+        
         return image;
     }
 };
