3cbdf670b08b2d651a92338e24de0e753ec039ea
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2023-08-27 23:09:39 +0530

    Added solution - LeetHub

--- Number of Provinces - GFG/number-of-provinces.cpp
+++ Number of Provinces - GFG/number-of-provinces.cpp
@@ -9,7 +9,7 @@ using namespace std;
 class Solution {
   public:
     
-    void dfs(int node,vector<int> adjList[],int vis[])
+    void dfs(int node,vector<int> adjList[],vector<int> &vis)
     {
         vis[node]=1;
         for(auto it:adjList[node])
@@ -20,23 +20,23 @@ class Solution {
             }
         }
     }
-    
+  
     int numProvinces(vector<vector<int>> adj, int V) {
-        int cnt=0;
-        vector<int> adjList[V];
+        vector<int> adjList[V+1];
+        vector<int> vis(V+1,0);
         for(int i=0;i<V;i++)
         {
             for(int j=0;j<V;j++)
             {
                 if(adj[i][j]==1 and i!=j)
                 {
-                    adjList[i].push_back(j);
-                    adjList[j].push_back(i);
+                    adjList[i+1].push_back(j+1);
+                    adjList[j+1].push_back(i+1);
                 }
             }
         }
-        int vis[V] = {0};
-        for(int i=0;i<V;i++)
+        int cnt=0;
+        for(int i=1;i<=V;i++)
         {
             if(!vis[i])
             {
