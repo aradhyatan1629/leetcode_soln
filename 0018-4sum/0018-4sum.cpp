362033bf67ef479e0324af1a00c15f6fafcb3281
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2023-08-28 16:55:49 +0530

    Time: 61 ms (78.17%), Space: 13.1 MB (94.93%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
-    //optimal soln - 
-    
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         sort(nums.begin(),nums.end());
-        int n=nums.size();
         vector<vector<int>> ans;
+        int n = nums.size();
+        
         for(int i=0;i<n;i++)
         {
             if(i>0 and nums[i]==nums[i-1])
@@ -22,25 +21,24 @@ public:
                 int l = n-1;
                 while(k<l)
                 {
-                    long long sum = nums[i];
-                    sum+=nums[j];
+                    long long sum = nums[i] + nums[j];
                     sum+=nums[k];
                     sum+=nums[l];
-                    if(sum==target)
+                    if(sum<target)
+                    {
+                        k++;
+                    }
+                    else if(sum>target)
+                    {
+                        l--;
+                    }
+                    else
                     {
                         ans.push_back({nums[i],nums[j],nums[k],nums[l]});
                         k++;
                         l--;
                         while(k<l and nums[k]==nums[k-1]){k++;}
                         while(k<l and nums[l]==nums[l+1]){l--;}
-                    }
-                    else if(sum<target)
-                    {
-                        k++;
-                    }
-                    else
-                    {
-                        l--;
                     }
                 }
             }
