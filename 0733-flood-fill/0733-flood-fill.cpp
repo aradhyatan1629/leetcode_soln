228a81c724da44a063654b64f61c32652544b040
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2023-08-28 20:19:34 +0530

    Time: 4 ms (90.93%), Space: 14.4 MB (10.87%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,18 +1,30 @@
 class Solution {
 public:
-    void dfs(int row,int col,vector<vector<int>> &image,vector<vector<int>> &vis,int delRow[],int delCol[],int color,int icolor)
+    
+    void bfs(int i,int j,vector<vector<int>> &image,vector<vector<int>> &vis,int initialColor,int color,int delRow[],int delCol[])
     {
-        int m=image.size();
-        int n=image[0].size();
-        vis[row][col] = 1;
-        image[row][col] = color;
-        for(int i=0;i<4;i++)
+        queue<pair<int,int>> q;
+        vis[i][j]=1;
+        image[i][j]=color;
+        q.push({i,j});
+        int m = image.size();
+        int n = image[0].size();
+        
+        while(!q.empty())
         {
-            int nrow = row + delRow[i];
-            int ncol = col + delCol[i];
-            if(nrow>=0 and nrow<m and ncol>=0 and ncol<n and image[nrow][ncol]==icolor and !vis[nrow][ncol])
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
+            for(int i=0;i<4;i++)
             {
-                dfs(nrow,ncol,image,vis,delRow,delCol,color,icolor);
+                int nrow = row + delRow[i];
+                int ncol = col + delCol[i];
+                if(nrow>=0 and nrow<m and ncol>=0 and ncol<n and image[nrow][ncol]==initialColor and !vis[nrow][ncol])
+                {
+                    vis[nrow][ncol] = 1;
+                    image[nrow][ncol] = color;
+                    q.push({nrow,ncol});
+                }
             }
         }
     }
@@ -21,12 +33,51 @@ public:
         int m = image.size();
         int n = image[0].size();
         vector<vector<int>> vis(m,vector<int>(n,0));
+        int initialColor = image[sr][sc];
         int delRow[] = {-1,0,+1,0};
         int delCol[] = {0,+1,0,-1};
-        int icolor = image[sr][sc];
         
-        dfs(sr,sc,image,vis,delRow,delCol,color,icolor);
-        
+        bfs(sr,sc,image,vis,initialColor,color,delRow,delCol);
         return image;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
