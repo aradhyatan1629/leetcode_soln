e427a222e458ff9235fffcbde73e68415552a14c
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2023-08-29 15:08:36 +0530

    Time: 7 ms (59.32%), Space: 13.4 MB (23.07%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,38 +1,41 @@
 class Solution {
 public:
+    
+    
     int orangesRotting(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        int cntfresh=0;
         vector<vector<int>> vis(m,vector<int>(n,0));
-        queue<pair<pair<int,int>,int>> q;   // store {{row, column}, time}
+        queue<pair<pair<int,int>,int>> q;
+        int delRow[] = {-1,0,+1,0};
+        int delCol[] = {0,+1,0,-1};
         
+        int cntFresh=0;
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
             {
-                if(grid[i][j]==2)// if cell contains rotten orange
+                if(grid[i][j]==2)
                 {
+                    vis[i][j] = 2;
                     q.push({{i,j},0});
-                    vis[i][j]=2;// mark as visited (rotten) in visited array
                 }
                 else if(grid[i][j]==1)
                 {
-                    cntfresh++;
+                    cntFresh++;
                 }
             }
         }
+        
         int time=0;
         int cnt=0;
-        int delRow[] = {-1,0,+1,0};
-        int delCol[] = {0,+1,0,-1};
         
         while(!q.empty())
         {
             int row = q.front().first.first;
             int col = q.front().first.second;
             int t = q.front().second;
-            time = max(t,time);
+            time = max(time,t);
             q.pop();
             
             for(int i=0;i<4;i++)
@@ -41,16 +44,37 @@ public:
                 int ncol = col + delCol[i];
                 if(nrow>=0 and nrow<m and ncol>=0 and ncol<n and grid[nrow][ncol]==1 and !vis[nrow][ncol])
                 {
-                    vis[nrow][ncol]=2;
-                    q.push({{nrow,ncol},t+1});  // push in queue with timer increased
+                    vis[nrow][ncol] = 2;
+                    q.push({{nrow,ncol},t+1});
                     cnt++;
                 }
             }
         }
-        if(cntfresh!=cnt)
+        if(cnt!=cntFresh)
         {
             return -1;
         }
         return time;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
