cef8270330dbb26c7dbba72bbecebe87bf04dc83
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2023-08-30 18:48:21 +0530

    Time: 26 ms (60.69%), Space: 17.2 MB (10.16%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,57 +1,52 @@
 class Solution {
 public:
-    
-    void dfs(int node,vector<int> adj[],vector<int> &vis)
+    //BFS soln - 
+    void bfs(int node,vector<int> adjList[],vector<int> &vis)
     {
-        vis[node] = 1;
-        for(auto it:adj[node])
+        queue<int> q;
+        vis[node]=1;
+        q.push(node);
+        while(!q.empty())
         {
-            if(!vis[it])
+            int node = q.front();
+            q.pop();
+            for(auto it:adjList[node])
             {
-                dfs(it,adj,vis);
+                if(!vis[it])
+                {
+                    vis[it]=1;
+                    q.push(it);
+                }
             }
         }
     }
     
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int n = isConnected.size();
-        vector<int> vis(n+1,0);
-        vector<int> adj[n+1];
+        int m = isConnected.size();
+        int n = isConnected[0].size();
+        vector<int> vis(m+1);
+        vector<int> adjList[m+1];
         
-        for(int i=0;i<n;i++)
+        for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
             {
-                if(isConnected[i][j]==1 and i!=j)
+                if(i!=j and isConnected[i][j]==1)
                 {
-                    adj[i+1].push_back(j+1);
-                    adj[j+1].push_back(i+1);
+                    adjList[i+1].push_back(j+1);
+                    adjList[j+1].push_back(i+1);
                 }
             }
         }
         int cnt=0;
-        for(int i=1;i<=n;i++)
+        for(int i=1;i<=m;i++)
         {
             if(!vis[i])
             {
                 cnt++;
-                dfs(i,adj,vis);
+                bfs(i,adjList,vis);
             }
         }
         return cnt;
     }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
