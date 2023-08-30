2c17486f669a012cd4c230e40071f4db46e8c76e
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2023-08-30 20:16:19 +0530

    Time: 7 ms (58.93%), Space: 13.4 MB (23.10%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
-    
-    
     int orangesRotting(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
         vector<vector<int>> vis(m,vector<int>(n,0));
-        queue<pair<pair<int,int>,int>> q;
         int delRow[] = {-1,0,+1,0};
         int delCol[] = {0,+1,0,-1};
+        queue<pair<pair<int,int>,int>> q;      //{{row,col},time}
         
         int cntFresh=0;
         for(int i=0;i<m;i++)
@@ -17,7 +15,7 @@ public:
             {
                 if(grid[i][j]==2)
                 {
-                    vis[i][j] = 2;
+                    vis[i][j]=2;
                     q.push({{i,j},0});
                 }
                 else if(grid[i][j]==1)
@@ -27,15 +25,14 @@ public:
             }
         }
         
-        int time=0;
         int cnt=0;
-        
+        int time=0;
         while(!q.empty())
         {
             int row = q.front().first.first;
             int col = q.front().first.second;
-            int t = q.front().second;
-            time = max(time,t);
+            int tm = q.front().second;
+            time = max(tm,time);
             q.pop();
             
             for(int i=0;i<4;i++)
@@ -44,8 +41,8 @@ public:
                 int ncol = col + delCol[i];
                 if(nrow>=0 and nrow<m and ncol>=0 and ncol<n and grid[nrow][ncol]==1 and !vis[nrow][ncol])
                 {
-                    vis[nrow][ncol] = 2;
-                    q.push({{nrow,ncol},t+1});
+                    vis[nrow][ncol]=2;
+                    q.push({{nrow,ncol},tm+1});
                     cnt++;
                 }
             }
@@ -57,6 +54,7 @@ public:
         return time;
     }
 };
+
 
 
 
