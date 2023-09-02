91bcf0354f4dd70882b3d35c55622005cf38e679
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2023-09-03 00:40:08 +0530

    Time: 11 ms (68.94%), Space: 10.6 MB (21.10%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,18 +1,28 @@
 class Solution {
 public:
     
-    void dfs(int row,int col,vector<vector<int>> &vis,vector<vector<char>> &board,int delRow[],int delCol[])
+    void bfs(int i,int j,vector<vector<int>> &vis,vector<vector<char>> &board,int delRow[],int delCol[])
     {
-        vis[row][col] = 1;
-        int m = board.size();
-        int n = board[0].size();
-        for(int i=0;i<4;i++)
+        queue<pair<int,int>> q;
+        vis[i][j] = 1;
+        q.push({i,j});
+        
+        int m = board.size(),n=board[0].size();
+        while(!q.empty())
         {
-            int nrow = row + delRow[i];
-            int ncol = col + delCol[i];
-            if(nrow>=0 and nrow<m and ncol>=0 and ncol<n and !vis[nrow][ncol] and board[nrow][ncol]=='O')
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
+            
+            for(int i=0;i<4;i++)
             {
-                dfs(nrow,ncol,vis,board,delRow,delCol);
+                int nrow = row + delRow[i];
+                int ncol = col + delCol[i];
+                if(nrow>=0 and nrow<m and ncol>=0 and ncol<n and !vis[nrow][ncol] and board[nrow][ncol]=='O')
+                {
+                    vis[nrow][ncol]=1;
+                    q.push({nrow,ncol});
+                }
             }
         }
     }
@@ -31,15 +41,16 @@ public:
             {
                 if((i==0 or i==m-1 or j==0 or j==n-1) and (!vis[i][j] and board[i][j]=='O'))
                 {
-                    dfs(i,j,vis,board,delRow,delCol);
+                    bfs(i,j,vis,board,delRow,delCol);
                 }
             }
         }
+        
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
             {
-                if(board[i][j]=='O' and vis[i][j]!=1)
+                if(board[i][j]=='O' and vis[i][j]==0)
                 {
                     board[i][j] = 'X';
                 }
