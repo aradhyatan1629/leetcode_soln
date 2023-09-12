1fe7e00c2e8b6602ce94552006574e9425d2c2c8
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2023-09-13 00:04:35 +0530

    Time: 6 ms (90.32%), Space: 14 MB (12.80%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
+        int n = groupSizes.size();
         unordered_map<int,vector<int>> m;
         vector<vector<int>> ans;
-        for(int i=0;i<groupSizes.size();i++)
+        
+        for(int i=0;i<n;i++)
         {
             m[groupSizes[i]].push_back(i);
             if(m[groupSizes[i]].size()==groupSizes[i])
             {
                 ans.push_back(m[groupSizes[i]]);
-                m[groupSizes[i]].clear();
+                m.erase(groupSizes[i]);
             }
         }
         return ans;
