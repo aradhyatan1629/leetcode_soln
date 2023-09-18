ec00a96ccbc1e178643410ec53a7062076232fdf
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2023-09-19 01:17:29 +0530

    Time: 18 ms (61.38%), Space: 13.5 MB (75.65%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,33 +1,13 @@
 class Solution {
 public:
-    
-    bool dfs(int node,vector<int> &vis,vector<int> &pathvis,vector<int> adj[])
-    {
-        vis[node]=1;
-        pathvis[node]=1;
-        for(auto it:adj[node])
-        {
-            if(!vis[it])
-            {
-                if(dfs(it,vis,pathvis,adj))
-                {
-                    return true;
-                }
-            }
-            else if(vis[it]==1 and pathvis[it]==1)
-            {
-                return true;
-            }
-        }
-        pathvis[node]=0;
-        return false;
-    }
+    /*
+    We need to find if there exists a cycle or not if there is a cycle we cannot complete all the tasks or else we can. To find a cycle use dfs or bfs(Topological sort)
+    */
     
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         int V = numCourses;
         vector<int> adj[V];
-        vector<int> vis(V,0);
-        vector<int> pathvis(V,0);
+        vector<int> indegree(V,0);
         
         for(int i=0;i<prerequisites.size();i++)
         {
@@ -38,14 +18,38 @@ public:
         
         for(int i=0;i<V;i++)
         {
-            if(!vis[i])
+            for(auto x:adj[i])
             {
-                if(dfs(i,vis,pathvis,adj))
+                indegree[x]++;
+            }
+        }
+        queue<int> q;
+        int cnt=0;
+        for(int i=0;i<indegree.size();i++)
+        {
+            if(indegree[i]==0)
+            {
+                q.push(i);
+            }
+        }
+        while(!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+            cnt++;
+            for(auto it:adj[node])
+            {
+                indegree[it]--;
+                if(indegree[it]==0)
                 {
-                    return false;
+                    q.push(it);
                 }
             }
         }
-        return true;
+        if(cnt==V)
+        {
+            return true;
+        }
+        return false;
     }
 };
