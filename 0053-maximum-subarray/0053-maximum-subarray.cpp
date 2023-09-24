ee40b495a9324bef696479b69a24ed52bd73296f
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2023-09-24 16:28:51 +0530

    Time: 82 ms (61.45%), Space: 68.2 MB (8.66%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     
-    //Kadane's Akgorithm
+    //kadane's algorithm O(n)
     int maxSubArray(vector<int>& nums) {
         int mx = INT_MIN;
         int sum=0;
-        
         for(int i=0;i<nums.size();i++)
         {
             sum+=nums[i];
-            if(sum > mx)
+            if(sum>mx)
             {
-                mx = sum;
+                mx=sum;
             }
-            if(sum<0)
+            if(sum<0) // if we carry negative sum forward, the total sum will always reduce
             {
                 sum=0;
             }
@@ -21,3 +20,5 @@ public:
         return mx;
     }
 };
+
+/*The intuition of the algorithm is not to consider the subarray as a part of the answer if its sum is less than 0. A subarray with a sum less than 0 will always reduce our answer and so this type of subarray cannot be a part of the subarray with maximum sum.*/
