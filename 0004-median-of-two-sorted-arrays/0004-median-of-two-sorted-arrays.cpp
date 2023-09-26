b2205637d7a436a2e26ab00bc91dcea7c92d0ff7
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2023-09-27 01:55:54 +0530

    Time: 26 ms (57.91%), Space: 89.6 MB (74.55%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,37 +1,50 @@
 class Solution {
 public:
+    //O(n) and O(1)
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         int n1=nums1.size(),n2=nums2.size();
-        vector<int> v;
+        int n = n1+n2;
+        int ind1=(n/2)-1;
+        int ind2=n/2;
+        int ind1e=-1,ind2e=-1;
+        int cnt=0;
         int i=0,j=0;
+        
         while(i<n1 and j<n2)
         {
             if(nums1[i]<=nums2[j])
             {
-                v.push_back(nums1[i]);
+                if(cnt==ind1){ind1e=nums1[i];}
+                if(cnt==ind2){ind2e=nums1[i];}
+                cnt++;
                 i++;
             }
             else
             {
-                v.push_back(nums2[j]);
+                if(cnt==ind1){ind1e=nums2[j];}
+                if(cnt==ind2){ind2e=nums2[j];}
+                cnt++;
                 j++;
             }
         }
         while(i<n1)
         {
-            v.push_back(nums1[i]);
+            if(cnt==ind1){ind1e=nums1[i];}
+            if(cnt==ind2){ind2e=nums1[i];}
+            cnt++;
             i++;
         }
         while(j<n2)
         {
-            v.push_back(nums2[j]);
+            if(cnt==ind1){ind1e=nums2[j];}
+            if(cnt==ind2){ind2e=nums2[j];}
+            cnt++;
             j++;
         }
-        int n = v.size();
         if(n%2==1)
         {
-            return (double)v[n/2];
+            return (double)ind2e;
         }
-        return double(double(v[(n/2)-1]+v[n/2])/2.0);
+        return double((double(ind1e+ind2e))/2.0);
     }
 };
