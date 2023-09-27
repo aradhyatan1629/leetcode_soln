634397cc0fb77781ea6ad7e60dd153d5fcef0cca
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2023-09-28 01:48:12 +0530

    Time: 26 ms (58.05%), Space: 25.6 MB (11.18%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -1,28 +1,42 @@
 class Solution {
 public:
-    //O(n^2)
     vector<int> nextGreaterElements(vector<int>& nums) {
+        stack<int> st;
         vector<int> ans;
-        int n = nums.size();
         
-        for(int i=0;i<n;i++)
+        for(int i=nums.size()-1;i>=0;i--)
         {
-            bool found=false;
-            for(int j=i+1;j<i+n;j++)
-            {
-                int index=j%n;
-                if(nums[index]>nums[i])
-                {
-                    found=true;
-                    ans.push_back(nums[index]);
-                    break;
-                }
-            }
-            if(!found)
+            st.push(nums[i]);
+        }
+        
+        for(int i=nums.size()-1;i>=0;i--)
+        {
+            if(st.empty())
             {
                 ans.push_back(-1);
             }
+            else if(!st.empty() and st.top()>nums[i])
+            {
+                ans.push_back(st.top());
+            }
+            else if(!st.empty() and st.top()<=nums[i])
+            {
+                while(!st.empty() and st.top()<=nums[i])
+                {
+                    st.pop();
+                }
+                if(st.empty())
+                {
+                    ans.push_back(-1);
+                }
+                else
+                {
+                    ans.push_back(st.top());
+                }
+            }
+            st.push(nums[i]);
         }
+        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
