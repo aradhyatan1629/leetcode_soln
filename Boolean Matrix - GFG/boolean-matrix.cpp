c2ebcf941f7fe7a2e71019bf3475a5d03a698d88
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2023-09-30 16:14:15 +0530

    Added solution - LeetHub

--- Boolean Matrix - GFG/boolean-matrix.cpp
+++ Boolean Matrix - GFG/boolean-matrix.cpp
@@ -12,28 +12,54 @@ class Solution
     //is 1 then all the cells in its ith row and jth column will become 1.
     void booleanMatrix(vector<vector<int> > &matrix)
     {
-        int m = matrix.size(),n=matrix[0].size();
-        vector<int> row(m,0),col(n,0);
-        
+        bool flagRow0=false,flagCol0=false;
+        int m=matrix.size(),n=matrix[0].size();
+        for(int j=0;j<n;j++)
+        {
+            if(matrix[0][j]==1)
+            {
+                flagRow0=true;
+            }
+        }
         for(int i=0;i<m;i++)
         {
-            for(int j=0;j<n;j++)
+            if(matrix[i][0]==1)
+            {
+                flagCol0=true;
+            }
+        }
+        for(int i=1;i<m;i++)
+        {
+            for(int j=1;j<n;j++)
             {
                 if(matrix[i][j]==1)
                 {
-                    row[i] = col[j] = 1;
+                    matrix[i][0] = matrix[0][j] = 1;
                 }
             }
         }
-        
-        for(int i=0;i<m;i++)
+        for(int i=1;i<m;i++)
         {
-            for(int j=0;j<n;j++)
+            for(int j=1;j<n;j++)
             {
-                if(row[i]==1 or col[j]==1)
+                if(matrix[i][0]==1 or matrix[0][j]==1)
                 {
                     matrix[i][j]=1;
                 }
+            }
+        }
+        if(flagRow0)
+        {
+            for(int j=0;j<n;j++)
+            {
+                matrix[0][j]=1;
+            }
+        }
+        if(flagCol0)
+        {
+            for(int i=0;i<m;i++)
+            {
+                matrix[i][0]=1;
             }
         }
     }
