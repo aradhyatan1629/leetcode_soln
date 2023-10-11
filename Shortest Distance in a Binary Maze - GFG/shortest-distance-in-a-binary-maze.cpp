d093d6a8ba6d20b96cdf84cafdf13d7072c026c0
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2023-10-11 22:25:44 +0530

    Added solution - LeetHub

--- Shortest Distance in a Binary Maze - GFG/shortest-distance-in-a-binary-maze.cpp
+++ Shortest Distance in a Binary Maze - GFG/shortest-distance-in-a-binary-maze.cpp
@@ -14,38 +14,38 @@ class Solution {
                      pair<int, int> destination) 
     {
         
-        if (source.first == destination.first &&
+         if (source.first == destination.first &&
             source.second == destination.second)
             return 0;
         
         
-        int n = grid.size(),m=grid[0].size();
-        queue<pair<int,pair<int,int>>> q;        //{dist,{row,col}}
+        int n=grid.size(),m=grid[0].size();
+        queue<pair<int,pair<int,int>>> q;  //{dist,{i,j}}
         vector<vector<int>> dist(n,vector<int>(m,1e9));
         
-        dist[source.first][source.second] = 0;
+        dist[source.first][source.second]=0;
+        q.push({0,{source.first,source.second}});
         
         int delRow[] = {-1,0,+1,0};
         int delCol[] = {0,+1,0,-1};
         
-        q.push({0,{source.first,source.second}});
-        
         while(!q.empty())
         {
-            int dis = q.front().first;
-            int row = q.front().second.first;
-            int col = q.front().second.second;
+            auto it = q.front();
             q.pop();
+            int dis = it.first;
+            int row = it.second.first;
+            int col = it.second.second;
             
             for(int i=0;i<4;i++)
             {
                 int nrow = row + delRow[i];
                 int ncol = col + delCol[i];
-                if(nrow>=0 and nrow<n and ncol>=0 and ncol<m and grid[nrow][ncol]==1 and dis+1 < dist[nrow][ncol])
+                if(nrow>=0 and nrow<n and ncol>=0 and ncol<m and grid[nrow][ncol]==1 and  dis+1<dist[nrow][ncol])
                 {
                     dist[nrow][ncol] = dis+1;
                     q.push({dis+1,{nrow,ncol}});
-                    if(nrow == destination.first and ncol == destination.second)
+                    if(nrow==destination.first and ncol==destination.second)
                     {
                         return dis+1;
                     }
