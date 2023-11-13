aac6b24756214f22dd659aed51a0d6eea8386ad9
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2023-11-13 15:59:19 +0530

    Time: 62 ms (73.11%), Space: 34.9 MB (20.54%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,28 +1,18 @@
 class Solution {
-public:   
-/*
-Intuition - 
-Bfs, 
-that is from each 0 we will move in the 4 directions and if 
-we get a 1 in any of the 4 direction then that 1 is at a distance of 1
-again we will go in 4 directions from these 1(which are at a distance of 1) , 
-now any 1 found now will be at a distance of 2 , and so on.
-*/
-    
+public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int m = mat.size();
-        int n = mat[0].size();
-        vector<vector<int>> dist(m,vector<int>(n,0));    //answer matrix
-        vector<vector<int>> vis(m,vector<int>(n,0));     //visited matrix
-        queue<pair<pair<int,int>,int>> q;                //{{row,col},distance}
+        int m=mat.size(),n=mat[0].size();
+        vector<vector<int>> ans(m,vector<int>(n,0));
+        vector<vector<int>> vis(m,vector<int>(n,0));
         
+        queue<pair<pair<int,int>,int>> q;
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
             {
-                if(mat[i][j] == 0)
+                if(mat[i][j]==0)
                 {
-                    vis[i][j] = 1;
+                    vis[i][j]=1;
                     q.push({{i,j},0});
                 }
             }
@@ -33,21 +23,21 @@ now any 1 found now will be at a distance of 2 , and so on.
         {
             int row = q.front().first.first;
             int col = q.front().first.second;
-            int distance = q.front().second;
+            int dist = q.front().second;
+            ans[row][col]=dist;
             q.pop();
-            dist[row][col] = distance;
             
             for(int i=0;i<4;i++)
             {
-                int nrow = row+delRow[i];
-                int ncol = col+delCol[i];
-                if(nrow>=0 and nrow<m and ncol>=0 and ncol<n and vis[nrow][ncol]==0)
+                int nrow = row + delRow[i];
+                int ncol = col + delCol[i];
+                if(nrow>=0 and nrow<m and ncol>=0 and ncol<n and mat[nrow][ncol]==1 and !vis[nrow][ncol])
                 {
-                    vis[nrow][ncol] = 1;
-                    q.push({{nrow,ncol},distance+1});
+                    vis[nrow][ncol]=1;
+                    q.push({{nrow,ncol},dist+1});
                 }
             }
         }
-        return dist;
+        return ans;
     }
 };
