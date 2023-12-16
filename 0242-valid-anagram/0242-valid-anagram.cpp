a7231c4f53ce2699f6fe0b03e053bad9a1d26206
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2023-12-17 01:25:41 +0530

    Time: 8 ms (62.94%), Space: 7.7 MB (79.33%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.size()!=t.size())
-        {
-            return false;
-        }
         vector<int> v(26,0);
         for(int i=0;i<s.size();i++)
-        {
             v[s[i]-'a']++;
-        }
+        
+        if(t.size()!=s.size())
+            return false;
         for(int i=0;i<t.size();i++)
         {
-            if(v[t[i]-'a']==0)
-            {
-                return false;
-            }
-            else
-            {
+            if(v[t[i]-'a']>0)
                 v[t[i]-'a']--;
-            }
+            else
+                return false;
         }
         return true;
     }
