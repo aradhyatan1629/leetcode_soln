b1d148200cce36e80eb34a491bcb0d564240a6f9
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2023-12-27 18:17:23 +0530

    Time: 11 ms (77.33%), Space: 26.6 MB (38.24%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -11,23 +11,20 @@
  */
 class Solution {
 public:
-    
-    TreeNode *solve(vector<int> &preorder,int preStart,int preEnd,vector<int> &inorder,int                           inStart,int inEnd,unordered_map<int,int> &m)
+    TreeNode* solve(vector<int> &preorder,int preStart,int preEnd,vector<int> &inorder,int inStart,int inEnd,unordered_map<int,int> &m)
     {
         if(preStart>preEnd or inStart>inEnd)
-        {
             return NULL;
-        }
-        TreeNode *root = new TreeNode(preorder[preStart]);
         
-        int RootInInorder = m[root->val];
-        int numsLeft = RootInInorder - inStart;
+        TreeNode *root = new TreeNode(preorder[preStart]);
+        int rootInInorder = m[root->val];
+        int numsLeft = rootInInorder - inStart;
         
         root->left = solve(preorder,preStart+1,preStart+numsLeft,
-                               inorder,inStart,RootInInorder-1,m);
+                           inorder,inStart,rootInInorder-1,m);
         
-        root->right = solve(preorder,preStart+numsLeft+1,preEnd,
-                               inorder,RootInInorder+1,inEnd,m);
+        root->right = solve(preorder,preStart+1+numsLeft,preEnd,
+                            inorder,rootInInorder+1,inEnd,m);
         
         return root;
     }
@@ -35,10 +32,12 @@ public:
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
         unordered_map<int,int> m;
         for(int i=0;i<inorder.size();i++)
-        {
             m[inorder[i]] = i;
-        }
+        
         TreeNode *root = solve(preorder,0,preorder.size()-1,inorder,0,inorder.size()-1,m);
+        
         return root;
+        
+        
     }
 };
