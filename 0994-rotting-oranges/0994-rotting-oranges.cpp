5bbaca86162d66972d7b456d1d2975434dfd90c2
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2024-01-07 20:09:16 +0530

    Time: 0 ms (100.00%), Space: 13.7 MB (37.91%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -3,8 +3,9 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int m = grid.size(),n=grid[0].size();
         vector<vector<int>> vis(m,vector<int>(n,0));
+        queue<pair<int,pair<int,int>>> q; //{time,{row,col}};
         
-        queue<pair<pair<int,int>,int>> q;
+        int cntFresh = 0;
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
@@ -12,44 +13,39 @@ public:
                 if(grid[i][j]==2)
                 {
                     vis[i][j]=1;
-                    q.push({{i,j},0});
+                    q.push({0,{i,j}});
+                }
+                else if(grid[i][j]==1)
+                {
+                    cntFresh++;
                 }
             }
         }
-        
         int delRow[] = {-1,0,+1,0};
         int delCol[] = {0,+1,0,-1};
         
-        int mx=0;
+        int mxTime = 0,cnt=0;
         while(!q.empty())
         {
-            int row = q.front().first.first;
-            int col = q.front().first.second;
-            int time = q.front().second;
-            mx=max(mx,time);
+            int time = q.front().first;
+            int row = q.front().second.first;
+            int col = q.front().second.second;
             q.pop();
-            
+            mxTime = max(mxTime,time);
             for(int i=0;i<4;i++)
             {
                 int nrow = row + delRow[i];
                 int ncol = col + delCol[i];
                 if(nrow>=0 and nrow<m and ncol>=0 and ncol<n and !vis[nrow][ncol] and grid[nrow][ncol]==1)
                 {
-                    vis[nrow][ncol]=1;
-                    q.push({{nrow,ncol},time+1});
+                    vis[nrow][ncol] = 1;
+                    q.push({time+1,{nrow,ncol}});
+                    cnt++;
                 }
             }
         }
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if(grid[i][j]==1 and vis[i][j]==0)
-                {
-                    return -1;
-                }
-            }
-        }
-        return mx;
+        if(cnt!=cntFresh)
+            return -1;
+        return mxTime;
     }
 };
