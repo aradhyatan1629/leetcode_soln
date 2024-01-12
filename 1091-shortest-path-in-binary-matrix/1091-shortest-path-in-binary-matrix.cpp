a02e3c12bc9b250626d8e68d9b838227da10f070
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2024-01-13 01:23:03 +0530

    Time: 56 ms (66.53%), Space: 22.6 MB (49.22%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int n = grid.size();
-        if(grid[0][0]!=0)
-        {
+        int n=grid.size();
+        if(grid[0][0]==1 or grid[n-1][n-1]==1)
             return -1;
-        }
-        
+        vector<vector<int>> vis(n,vector<int>(n,0));
         queue<pair<int,pair<int,int>>> q;
-        vector<vector<int>> dist(n,vector<int>(n,1e9));
-        
-        dist[0][0] = 1;
-        q.push({1,{0,0}});
+        vis[0][0]=1;
+        q.push({0,{0,0}});
         
         int delRow[] = {-1,0,+1,0,-1,+1,-1,+1};
         int delCol[] = {0,+1,0,-1,+1,+1,-1,-1};
@@ -21,22 +17,20 @@ public:
             int dis = q.front().first;
             int row = q.front().second.first;
             int col = q.front().second.second;
+            if(row==n-1 and col==n-1)
+                return dis+1;
             q.pop();
-            
             for(int i=0;i<8;i++)
             {
                 int nrow = row + delRow[i];
                 int ncol = col + delCol[i];
-                
-                
-                if(nrow>=0 and nrow<n and ncol>=0 and ncol<n and grid[nrow][ncol]==0 and dis+1<dist[nrow][ncol])
+                if(nrow>=0 and nrow<n and ncol>=0 and ncol<n and !vis[nrow][ncol] and grid[nrow][ncol]==0)
                 {
-                    dist[nrow][ncol] = 1 + dis;
+                    vis[nrow][ncol]=1;
                     q.push({dis+1,{nrow,ncol}});
                 }
             }
         }
-        if(dist[n-1][n-1]==1e9){return -1;}
-        return dist[n-1][n-1];
+        return -1;
     }
 };
