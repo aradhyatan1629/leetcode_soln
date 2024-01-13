af27584e1d7a4e99bedf1bcd90a3f72437222138
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2024-01-13 13:52:48 +0530

    Time: 92 ms (73.14%), Space: 40.2 MB (82.45%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -3,13 +3,10 @@ public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
         vector<pair<int,int>> adj[n+1];
         for(int i=0;i<times.size();i++)
-        {
-            adj[times[i][0]].push_back({times[i][2],times[i][1]});
-        }
+            adj[times[i][0]].push_back({times[i][1],times[i][2]});
         
         vector<int> dist(n+1,1e9);
         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-        
         dist[k]=0;
         pq.push({0,k});
         
@@ -18,25 +15,25 @@ public:
             int dis = pq.top().first;
             int node = pq.top().second;
             pq.pop();
-            
             for(auto it:adj[node])
             {
-                int edgeWt = it.first;
-                int adjNode = it.second;
-                if(dis + edgeWt < dist[adjNode])
+                int adjNode = it.first;
+                int adjDist = it.second;
+                if(dis + adjDist < dist[adjNode])
                 {
-                    dist[adjNode] = dis+edgeWt;
-                    pq.push({dis+edgeWt,adjNode});
+                    dist[adjNode] = dis+adjDist;
+                    pq.push({dis+adjDist,adjNode});
                 }
             }
         }
-        int mn=-1;
-        for(int i=1;i<dist.size();i++)
+        int mn=INT_MIN;
+        for(int i=1;i<=n;i++)
         {
-            mn = max(mn,dist[i]);
+            if(dist[i]==1e9)
+                return -1;
+            else
+                mn = max(mn,dist[i]);
         }
-        if(mn==1e9)
-            return -1;
         return mn;
     }
 };
