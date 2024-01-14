6cf8da69983dd176471ced2c17a4fe25397b352d
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2024-01-14 18:58:55 +0530

    Time: 27 ms (84.76%), Space: 19.3 MB (40.28%) - LeetHub

---
+++ 2521-distinct-prime-factors-of-product-of-array/2521-distinct-prime-factors-of-product-of-array.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    unordered_set<int> st;
+    void PrimeFactors(int n)
+    {
+        while(n%2 == 0)
+        {
+            if(st.find(2)==st.end())
+                st.insert(2);
+            n=n/2;
+        }
+        for(int i=3;i<=sqrt(n);i++)
+        {
+            while(n%i == 0)
+            {
+                if(st.find(i)==st.end())
+                    st.insert(i);
+                n=n/i;
+            }
+        }
+        if(n>2)
+            st.insert(n);
+    }
+    
+    int distinctPrimeFactors(vector<int>& nums) {
+        for(int i=0;i<nums.size();i++)
+        {
+            PrimeFactors(nums[i]);
+        }
+        for(auto x:st)
+            cout<<x<<" ";
+        return st.size();
+    }
+};
