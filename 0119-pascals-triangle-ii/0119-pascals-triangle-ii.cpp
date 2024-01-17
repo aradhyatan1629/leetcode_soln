ace83d98d2c591aebfc8ee380a8ba73bdcdff79b
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2024-01-17 18:34:32 +0530

    Time: 3 ms (40.98%), Space: 6.7 MB (69.58%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int findNCR(int n,int r)
+    {
+        long long ans=1;
+        for(int i=0;i<r;i++)
+        {
+            ans = ans * (n-i);
+            ans = ans/(i+1);
+        }
+        return ans;
+    }
+    
+    vector<int> getRow(int rowIndex) {
+        int n=rowIndex;
+        vector<int> v;
+        for(int c=0;c<=n;c++)
+        {
+            v.push_back(findNCR(n,c));
+        }
+        return v;
+    }
+};
+
+/*
+
+*/
