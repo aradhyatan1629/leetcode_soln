cd42bcb57c3f70727492490c7db6cabfc10d97e0
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2024-01-18 16:45:55 +0530

    Time: 3 ms (74.60%), Space: 10 MB (12.72%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,33 +1,22 @@
 class Solution {
 public:
-    bool binarySearch(vector<int> &v,int target)
-    {
-        int low=0,high=v.size()-1;
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m=matrix.size(),n=matrix[0].size();
+        int low=0,high=(m*n)-1;
         while(low<=high)
         {
             int mid = low + (high-low)/2;
-            if(v[mid]==target)
+            int row = mid/n;
+            int col = mid%n;
+            if(matrix[row][col]==target)
                 return true;
-            else if(v[mid]>target)
+            else if(matrix[row][col] > target)
                 high=mid-1;
             else
                 low=mid+1;
         }
         return false;
     }
-    
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int m=matrix.size(),n=matrix[0].size();
-        int i=0,j=n-1;
-        for(int i=0;i<m;i++)
-        {
-            if(matrix[i][0]<=target and matrix[i][n-1]>=target)
-            {
-                return binarySearch(matrix[i],target);
-            }
-        }
-        return false;
-    }
 };
 
-// O(m + logn) , we will check if the target lies in the current row is yes then we will apply binary search only on that row.
+// O(log(m*n)), we are treating the whole 2d array as a single 1d array, due to the nature of the question the 1d array will be sorted. We get corresponding matrix values for each index and check with target.
