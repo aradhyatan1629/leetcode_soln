6d37b902386ffd2ed53a8c796b12251532b11a47
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2024-01-18 16:24:12 +0530

    Time: 3 ms (74.60%), Space: 10 MB (24.79%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,19 +1,33 @@
 class Solution {
 public:
+    bool binarySearch(vector<int> &v,int target)
+    {
+        int low=0,high=v.size()-1;
+        while(low<=high)
+        {
+            int mid = low + (high-low)/2;
+            if(v[mid]==target)
+                return true;
+            else if(v[mid]>target)
+                high=mid-1;
+            else
+                low=mid+1;
+        }
+        return false;
+    }
+    
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int m=matrix.size(),n=matrix[0].size();
         int i=0,j=n-1;
-        while(i<m and j>=0)
+        for(int i=0;i<m;i++)
         {
-            if(matrix[i][j]==target)
-                return true;
-            else if(matrix[i][j]<target)
-                i++;
-            else
-                j--;
+            if(matrix[i][0]<=target and matrix[i][n-1]>=target)
+            {
+                return binarySearch(matrix[i],target);
+            }
         }
         return false;
     }
 };
 
-// O(m+n)
+// O(m + logn) , we will check if the target lies in the current row is yes then we will apply binary search only on that row.
