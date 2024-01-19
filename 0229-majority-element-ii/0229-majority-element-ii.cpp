7ef637b9eb36e3fcbb01f8e73fbfb7972de51759
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2024-01-19 14:35:39 +0530

    Time: 4 ms (94.44%), Space: 16.2 MB (98.02%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int cnt1=0,cnt2=0;
-        int element1,element2;
+        int cnt1=0,cnt2=0,element1,element2;
         for(int i=0;i<nums.size();i++)
         {
             if(cnt1==0 and nums[i]!=element2)
             {
                 cnt1=1;
-                element1=nums[i];
+                element1 = nums[i];
+            }
+            else if(element1 == nums[i])
+            {
+                cnt1++;
             }
             else if(cnt2==0 and nums[i]!=element1)
             {
                 cnt2=1;
-                element2=nums[i];
+                element2 = nums[i];
             }
-            else if(nums[i]==element1)
-            {
-                cnt1++;
-            }
-            else if(nums[i]==element2)
+            else if(element2 == nums[i])
             {
                 cnt2++;
             }
@@ -30,27 +29,14 @@ public:
             }
         }
         cnt1=0,cnt2=0;
-        int t = nums.size()/3;
         for(int i=0;i<nums.size();i++)
         {
-            if(nums[i]==element1)
-            {
-                cnt1++;
-            }
-            else if(nums[i]==element2)
-            {
-                cnt2++;
-            }
+            if(nums[i]==element1)cnt1++;
+            if(nums[i]==element2)cnt2++;
         }
         vector<int> ans;
-        if(cnt1>t)
-        {
-            ans.push_back(element1);
-        }
-        if(cnt2>t)
-        {
-            ans.push_back(element2);
-        }
+        if(cnt1>nums.size()/3)ans.push_back(element1);
+        if(cnt2>nums.size()/3)ans.push_back(element2);
         return ans;
     }
 };
