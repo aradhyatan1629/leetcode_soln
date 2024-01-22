1bf1e11ac01ade080b6472d7c7f3caa6f32123ca
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2024-01-22 20:18:46 +0530

    Time: 48 ms (34.22%), Space: 34.4 MB (8.31%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
+        int n=nums.size();
         unordered_map<int,int> m;
         vector<int> ans;
         for(int i=0;i<nums.size();i++)
         {
-            if(m[nums[i]]>=1)
-            {
-                ans.push_back(nums[i]);
-            }
             m[nums[i]]++;
+            if(m[nums[i]]==2)
+                ans.push_back(nums[i]);
         }
-        for(int i=1;i<=nums.size();++i)
-        {
+        for(int i=1;i<=n;i++)
             if(m[i]==0)
-            {
                 ans.push_back(i);
-                break;
-            }
-        }
         return ans;
     }
 };
