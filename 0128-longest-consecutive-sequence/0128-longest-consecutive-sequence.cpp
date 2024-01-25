a2a8396152639c3fa9d5030e2b20cd001c61c9e1
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2024-01-25 22:55:06 +0530

    Time: 125 ms (73.26%), Space: 74.1 MB (12.20%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,23 +1,39 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        if(nums.size()==0)return 0;
-        sort(nums.begin(),nums.end());
-        int len=1,mx=0;
-        for(int i=0;i<nums.size()-1;i++)
+        unordered_set<int> s;
+        for(int i=0;i<nums.size();i++)
+            s.insert(nums[i]);
+        int mxLen=0;
+        for(auto x:s)
         {
-            if(abs(nums[i]-nums[i+1])>1)
+            if(s.find(x-1)==s.end())
             {
-                mx = max(mx,len);
-                len=1;
-            }
-            else if(abs(nums[i]-nums[i+1])==1)
-            {
-                len++;
+                int cnt=1;
+                int t = x+1;
+                while(s.find(t)!=s.end())
+                {
+                    cnt++;
+                    t++;
+                }
+                mxLen=max(mxLen,cnt);
             }
         }
-        mx = max(mx,len);
-        return mx;
+        return mxLen;
     }
-}; 
-// tc - O(nlogn)
+};
+
+//tc - O(n) + O(2n) , under the assumption our unordered_set is taking O(1) for all operations.
+
+/*
+basically the intution is as follows - 
+if you are at a number say 3 then normally you would go ahead
+to check the consecutive sequence like from 3 to 4 to 5 to 6 etc
+but in optimized approach say we have a consecutive sequence like 1,2,3,4
+and u are at 3 now instead of going forward we will check whether we have
+3-1 i.e 2 in the set? if yes then 3 is not the STARTING POINT of the sequence
+similarly for 2 , 2-1=1 which is there in the set 
+but for 1, 1-1=0 which is not there in the set.
+hence we will start the linear search from 1 to count the length of consecutive sequence
+1,1+1=2,2+1=3,3+1=4. hence largest consecutive sequence length = 4
+*/
