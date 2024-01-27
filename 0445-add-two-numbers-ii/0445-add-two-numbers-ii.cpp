c63ed1b5f5ad8b9ec6049d7b1ed499f260b64fa6
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2024-01-27 16:42:44 +0530

    Time: 29 ms (33.65%), Space: 75.8 MB (7.89%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
@@ -10,65 +10,47 @@
  */
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-//         Using stack for additon and reversing the answer list using sliding pointers
-        stack<int> st1,st2;
-        ListNode *temp1=l1,*temp2=l2;
-        ListNode *l=new ListNode;   //list which contains our answer
-        ListNode *temp=l;
-        while(temp1!=NULL)
-        {
-            st1.push(temp1->val);
-            temp1=temp1->next;
-        }
-        while(temp2!=NULL)
-        {
-            st2.push(temp2->val);
-            temp2=temp2->next;
-        }
-        int sum=0,carry=0;
-        while(st1.size() or st2.size() or carry)
-        {
-            if(st1.size()!=0)
-            {
-                sum+=st1.top();
-                st1.pop();
-            }
-            if(st2.size()!=0)
-            {
-                sum+=st2.top();
-                st2.pop();
-            }
-            sum+=carry;
-            if(sum>=10)
-            {
-                carry=sum/10;
-                ListNode *ptr = new ListNode(sum%10);
-                temp->next=ptr;
-                temp=temp->next;
-            }
-            else
-            {
-                ListNode *ptr = new ListNode(sum);
-                temp->next=ptr;
-                temp=temp->next;
-                carry=0;
-            }
-            sum=0;
-        }
-        temp=l;
-        l=l->next;
-        temp->next=NULL;
-        temp=l;
-        ListNode *p=l,*q=NULL,*r=NULL;
-        while(p!=NULL)
+    
+    ListNode *reverseList(ListNode *head)
+    {
+        ListNode *p=NULL,*q=NULL,*r=NULL;
+        p=head;
+        while(p)
         {
             r=q;
             q=p;
             p=p->next;
             q->next=r;
         }
-        l=q;
-        return l;
+        head=q;
+        return head;
+    }
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        l1 = reverseList(l1),l2 = reverseList(l2);
+        ListNode *dummy = new ListNode();
+        ListNode *curr = dummy;
+        int carry=0;
+        while(l1 or l2 or carry!=0)
+        {
+            int sum=0;
+            if(l1)
+            {
+                sum+=l1->val;
+                l1=l1->next;
+            }
+            if(l2)
+            {
+                sum+=l2->val;
+                l2=l2->next;
+            }
+            sum+=carry;
+            carry=sum/10;
+            ListNode *temp = new ListNode(sum%10);
+            curr->next=temp;
+            curr=curr->next;
+        }
+        return reverseList(dummy->next);
     }
 };
+
+
