a630e6b1fde78e91412f84c2b338ac6d8e42094d
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2024-01-28 00:12:44 +0530

    Time: 0 ms (100.00%), Space: 15.2 MB (9.40%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -10,56 +10,29 @@
  */
 class Solution {
 public:
-    ListNode *reverseList(ListNode *head)
-    {
-        ListNode *p=NULL,*q=NULL,*r=NULL;
-        p=head;
-        while(p)
-        {
-            r=q;
-            q=p;
-            p=p->next;
-            q->next=r;
-        }
-        head=q;
-        return head;
-    }
-    
     ListNode* rotateRight(ListNode* head, int k) {
         if(!head or !head->next)return head;
         int n=0;
-        ListNode *temp=head;
+        ListNode *temp = head,*tail=NULL;
         while(temp)
         {
             n++;
-            temp=temp->next;
+            tail = temp;
+            temp = temp->next;
         }
         k%=n;
         if(k==0)return head;
-        head=reverseList(head);
-        temp=head;
-        int t=1;
-        while(t<k)
+        tail->next = head;
+        int t=1;temp=head;
+        while(t<n-k)
         {
             t++;
-            temp = temp->next;
+            temp=temp->next;
         }
-        ListNode *temp2 = temp->next;
-        temp->next=NULL;
-        ListNode *newHead1 = reverseList(head);
-        ListNode *newHead2 = reverseList(temp2);
-        head->next=newHead2;
-        return newHead1;
+        head = temp->next;
+        temp->next = NULL;
+        return head;
     }
 };
 
-// Using same concept as rotate array 
-
-
-
-
-
-
-
-
-
+// O(n) O(1)
