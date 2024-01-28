37971112346d3654008204e78fa7d054b628aec3
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2024-01-29 02:24:32 +0530

    Time: 19 ms (68.07%), Space: 24.7 MB (12.24%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -6,14 +6,18 @@ public:
         {
             int mid = low + (high-low)/2;
             if(mid%2==0 and nums[mid]==nums[mid+1] or mid%2==1 and nums[mid]==nums[mid-1])
-            {
                 low=mid+1;
-            }
             else
-            {
                 high=mid;
-            }
         }
         return nums[low];
     }
 };
+
+
+
+/*
+0,1,2,3,4,5,6,7,8
+1,1,2,3,3,4,4,8,8
+
+*/
