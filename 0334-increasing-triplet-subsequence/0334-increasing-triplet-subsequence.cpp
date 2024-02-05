7d4acada4bf72d9c452430ca9c2941827b915117
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2024-02-06 02:38:25 +0530

    Time: 316 ms (5.04%), Space: 187.5 MB (6.24%) - LeetHub

---
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        int n=nums.size();
+        // vector<int> nsl,ngr;
+        unordered_map<int,int> nsl,ngr;
+        stack<int> st;
+        for(int i=0;i<n;i++)
+        {
+            while(!st.empty() and st.top()>=nums[i])
+                st.pop();
+            if(!st.empty())
+                nsl[i] = st.top();
+            // else
+            //     nsl.push_back(st.top());
+            st.push(nums[i]);
+        }
+        while(!st.empty())
+            st.pop();
+        for(int i=n-1;i>=0;i--)
+        {
+            while(!st.empty() and st.top()<=nums[i])
+                st.pop();
+            if(!st.empty())
+                ngr[i] = st.top();
+            // else
+            //     ngr.push_back(st.top());
+            st.push(nums[i]);
+        }
+        // reverse(ngr.begin(),ngr.end());
+        for(int i=0;i<n;i++)
+        {
+            if(nsl.find(i)!=nsl.end() and ngr.find(i)!=ngr.end())
+                return true;
+        }
+        return false;
+    }
+};
