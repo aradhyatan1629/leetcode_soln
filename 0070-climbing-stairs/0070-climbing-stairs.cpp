2e6938dd6859bba1ed6e4d1548ac4eb9fb6e2291
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2024-02-22 00:38:59 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (10.47%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
-    //Memoization O(n)
-    
-    int count(int n,vector<int> &dp)
+    int solve(int n,vector<int> &dp)
     {
-        if(n==0 or n==1)
-        {
-            return 1;
-        }
+        if(n<=2)
+            return n;
         if(dp[n]!=-1)
-        {
             return dp[n];
-        }
-        dp[n] = count(n-1,dp) + count(n-2,dp);
+        dp[n] = solve(n-1,dp)+solve(n-2,dp);
         return dp[n];
     }
     
     int climbStairs(int n) {
+        if(n<=2)
+            return n;
         vector<int> dp(n+1,-1);
-        return count(n,dp);
+        return solve(n,dp);
     }
 };
