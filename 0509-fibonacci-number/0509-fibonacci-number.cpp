65fe566ca7207b604bef3488bd23392dad7bfe3e
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2024-02-21 21:04:15 +0530

    Time: 2 ms (77.63%), Space: 7 MB (38.17%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,10 +1,17 @@
 class Solution {
 public:
     int fib(int n) {
+        int a=0,b=1,c;
         if(n<=1)
-        {
             return n;
+        for(int i=2;i<=n;i++)
+        {
+            c=a+b;
+            a=b;
+            b=c;
         }
-        return fib(n-1)+fib(n-2);
+        return c;
     }
 };
+
+// Iterative O(n) and O(1)
