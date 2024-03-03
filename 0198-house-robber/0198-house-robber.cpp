e902a1fd57da40644bc29effd9564c18b893f87e
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2024-03-04 01:20:23 +0530

    Time: 0 ms (100.00%), Space: 9.2 MB (26.29%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-    int solve(int ind,vector<int> &nums,vector<int> &dp)
-    {
-        if(ind==0)
-            return nums[ind];
-        if(ind<0)
-            return 0;
-        if(dp[ind]!=-1)
-            return dp[ind];
-        int pick = nums[ind] + solve(ind-2,nums,dp);
-        int notPick = 0 + solve(ind-1,nums,dp);
-        return dp[ind] = max(pick,notPick);
-    }
-    
     int rob(vector<int>& nums) {
-        int n=nums.size();
+        int n = nums.size();
         vector<int> dp(n,-1);
-        return solve(n-1,nums,dp);
+        dp[0] = nums[0];
+        int neg=0;
+        for(int i=1;i<n;i++)
+        {
+            int pick = nums[i];
+            if(i>1)
+                pick += dp[i-2];
+            int notPick = 0 + dp[i-1];
+            dp[i] = max(pick,notPick);
+        }
+        return dp[n-1];
     }
 };
 
-// memoization soln - t.c. - O(n)
 
+// Tabulation method - O(n) and O(n)
