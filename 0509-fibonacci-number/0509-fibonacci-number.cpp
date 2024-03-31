77fb1a86ccd2bfaf6d4d27f0a7a75c1c615943f8
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2024-03-31 17:47:45 +0530

    Time: 9 ms (53.38%), Space: 7.1 MB (73.59%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
-    int solve(int n,vector<int> &dp)
+    int solve(int n)
     {
         if(n<=1)
             return n;
-        if(dp[n]!=-1)
-            return dp[n];
-        return dp[n] = solve(n-1,dp)+solve(n-2,dp);
+        return solve(n-1)+solve(n-2); 
     }
     
     int fib(int n) {
-        if(n<=1)return n;
-        vector<int> dp(n+1,-1);
-        dp[0]=0,dp[1]=1;
-        solve(n,dp);
-        return dp[n];
+        return solve(n);
     }
 };
 
-// Dp using memoization, O(n) and O(n) space
+// Recursion soln.
+// O(n) time O(n) space
