883ebe8744336f6de668d0c6dfb5415bbb9c5f6b
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2024-04-02 16:38:10 +0530

    Time: 7 ms (59.26%), Space: 12.4 MB (60.39%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,26 +1,34 @@
 class Solution {
 public:
-    int solve(int i,int j,vector<vector<int>> &dp,vector<vector<int>> &grid)
-    {
-        if(i==0 and j==0)
-            return grid[0][0];
-        if(i<0 or j<0)
-            return 1e9;
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        int up = grid[i][j] + solve(i-1,j,dp,grid);
-        int left = grid[i][j] + solve(i,j-1,dp,grid);
-        dp[i][j] = min(up,left);
-        return dp[i][j];
-    }
-    
     int minPathSum(vector<vector<int>>& grid) {
         int m=grid.size(),n=grid[0].size();
         vector<vector<int>> dp(m,vector<int>(n,-1));
-        return solve(m-1,n-1,dp,grid);
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i==0 and j==0)
+                {
+                    dp[i][j] = grid[i][j];
+                    continue;
+                }
+                int up = grid[i][j];
+                if(i>0)
+                    up += dp[i-1][j];
+                else
+                    up = 1e9;
+                int left = grid[i][j];
+                if(j>0)
+                    left += dp[i][j-1];
+                else
+                    left = 1e9;
+                dp[i][j] = min(up,left);
+            }
+        }
+        return dp[m-1][n-1];
     }
 }; 
 
-// Memoization
+// Tabulation
 // t.c. - O(m+n)
-// s.c. - O(m+n) + O(m*n),recursion stack space and extra dp array
+// s.c. - O(m*n) extra dp array
