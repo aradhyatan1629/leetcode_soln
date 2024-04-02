be80361c8a7e08b6ca2cb9f50f2f16c133e1bd8c
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2024-04-02 14:46:55 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (17.74%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -4,16 +4,17 @@ public:
     {
         if(i==0 and j==0)
             return 1;
-        if(i<0 or j<0)
+        if(i<0 || j<0)
             return 0;
         if(dp[i][j]!=-1)
             return dp[i][j];
-        dp[i][j] = solve(i-1,j,dp) + solve(i,j-1,dp);
-        return dp[i][j];
+        int up = solve(i-1,j,dp);
+        int left = solve(i,j-1,dp);
+        return dp[i][j] = up+left;
     }
     
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m,vector<int>(n,-1));
-        return solve(m-1,n-1,dp); 
+        return solve(m-1,n-1,dp);
     }
 };
