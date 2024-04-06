659c909b894ad21f5ed6feb7e7a8cd88db9c4b10
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2024-04-06 22:51:38 +0530

    Time: 7 ms (42.55%), Space: 16.1 MB (42.62%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -3,49 +3,45 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int m = grid.size(),n=grid[0].size();
         vector<vector<int>> vis(m,vector<int>(n,0));
-        queue<pair<int,pair<int,int>>> q; //{time,{row,col}};
-        
+        queue<pair<int,pair<int,int>>> q;
         int cntFresh = 0;
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
             {
-                if(grid[i][j]==2)
+                if(grid[i][j]==2 and !vis[i][j])
                 {
                     vis[i][j]=1;
                     q.push({0,{i,j}});
                 }
                 else if(grid[i][j]==1)
-                {
                     cntFresh++;
-                }
             }
         }
         int delRow[] = {-1,0,+1,0};
         int delCol[] = {0,+1,0,-1};
-        
-        int mxTime = 0,cnt=0;
+        int time = 0,cnt=0;
         while(!q.empty())
         {
-            int time = q.front().first;
+            int tm = q.front().first;
+            time = max(time,tm);
             int row = q.front().second.first;
             int col = q.front().second.second;
             q.pop();
-            mxTime = max(mxTime,time);
             for(int i=0;i<4;i++)
             {
                 int nrow = row + delRow[i];
                 int ncol = col + delCol[i];
                 if(nrow>=0 and nrow<m and ncol>=0 and ncol<n and !vis[nrow][ncol] and grid[nrow][ncol]==1)
                 {
-                    vis[nrow][ncol] = 1;
-                    q.push({time+1,{nrow,ncol}});
                     cnt++;
+                    vis[nrow][ncol] = 1;
+                    q.push({tm+1,{nrow,ncol}});
                 }
             }
         }
-        if(cnt!=cntFresh)
+        if(cntFresh != cnt)
             return -1;
-        return mxTime;
+        return time;
     }
 };
