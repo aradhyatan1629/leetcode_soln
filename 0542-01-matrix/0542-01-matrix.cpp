53c17c39b1623133984a852c0cc2306cb745f73f
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2024-04-06 23:50:10 +0530

    Time: 52 ms (66.48%), Space: 35.3 MB (65.66%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int m=mat.size(),n=mat[0].size();
-        vector<vector<int>> ans(m,vector<int>(n,0));
+        int m = mat.size();
+        int n = mat[0].size();
         vector<vector<int>> vis(m,vector<int>(n,0));
-        
-        queue<pair<pair<int,int>,int>> q;
+        queue<pair<int,pair<int,int>>> q;
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
             {
-                if(mat[i][j]==0)
+                if(!vis[i][j] and mat[i][j]==0)
                 {
                     vis[i][j]=1;
-                    q.push({{i,j},0});
+                    q.push({0,{i,j}});
                 }
             }
         }
@@ -21,23 +20,29 @@ public:
         int delCol[] = {0,+1,0,-1};
         while(!q.empty())
         {
-            int row = q.front().first.first;
-            int col = q.front().first.second;
-            int dist = q.front().second;
-            ans[row][col]=dist;
+            int dist = q.front().first;
+            int row = q.front().second.first;
+            int col = q.front().second.second;
             q.pop();
-            
             for(int i=0;i<4;i++)
             {
                 int nrow = row + delRow[i];
                 int ncol = col + delCol[i];
-                if(nrow>=0 and nrow<m and ncol>=0 and ncol<n and mat[nrow][ncol]==1 and !vis[nrow][ncol])
+                if(nrow>=0 and nrow<m and ncol>=0 and ncol<n and !vis[nrow][ncol] and mat[nrow][ncol]==1)
                 {
-                    vis[nrow][ncol]=1;
-                    q.push({{nrow,ncol},dist+1});
+                    vis[nrow][ncol] = dist+1;
+                    q.push({dist+1,{nrow,ncol}});
                 }
             }
         }
-        return ans;
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(mat[i][j]==0)
+                    vis[i][j]=0;
+            }
+        }
+        return vis;
     }
 };
