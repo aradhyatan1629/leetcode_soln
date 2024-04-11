fa50a0595be09e7e3f9c512906ffade7ac42eb9e
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2024-04-11 16:31:37 +0530

    Time: 0 ms (100.00%), Space: 9.4 MB (26.75%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -2,34 +2,32 @@ class Solution {
 public:
     int solve(int ind,vector<int> &dp,vector<int> &nums)
     {
-        if(ind == 0)
-        {
-            return nums[ind];
-        }
+        if(ind==0)
+            return nums[0];
         if(ind<0)
             return 0;
         if(dp[ind]!=-1)
             return dp[ind];
-        int notTake = 0 + solve(ind-1,dp,nums);
         int take = nums[ind] + solve(ind-2,dp,nums);
+        int notTake = 0 + solve(ind-1,dp,nums);
         dp[ind] = max(take,notTake);
         return dp[ind];
     }
     
     int rob(vector<int>& nums) {
         int n = nums.size();
-        if(n==1)
-            return nums[0];
-        
+        if(n==1)return nums[0];
         vector<int> v1,v2;
-        for(int i=0;i<nums.size();i++)
+        for(int i=0;i<n;i++)
         {
-            if(i!=0)v1.push_back(nums[i]);
-            if(i!=n-1)v2.push_back(nums[i]);
+            if(i!=n-1)
+                v1.push_back(nums[i]);
+            if(i!=0)
+                v2.push_back(nums[i]);
         }
-        vector<int> dp(n,-1);
+        vector<int> dp(n+1,-1);
         int s1 = solve(v1.size()-1,dp,v1);
-        for(int i=0;i<dp.size();i++)dp[i]=-1;
+        for(auto &x:dp)x=-1;
         int s2 = solve(v2.size()-1,dp,v2);
         return max(s1,s2);
     }
