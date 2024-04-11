c3ad6f1cc55a3ac1ecdbca4c5a68a52aa06bd4df
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2024-04-11 18:54:44 +0530

    Time: 0 ms (100.00%), Space: 11.5 MB (8.45%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -4,7 +4,7 @@ public:
     {
         if(i==m-1)
             return triangle[i][j];
-        if(dp[i][j]!=-1)
+        if(dp[i][j]!=1e9)
             return dp[i][j];
         int down = triangle[i][j] + solve(i+1,j,m,dp,triangle);
         int diagonal = triangle[i][j] + solve(i+1,j+1,m,dp,triangle);
@@ -14,9 +14,7 @@ public:
     
     int minimumTotal(vector<vector<int>>& triangle) {
         int m = triangle.size();
-        vector<vector<int>> dp(m,vector<int>(m,-1));
+        vector<vector<int>> dp(m,vector<int>(m,1e9));
         return solve(0,0,m,dp,triangle);
     }
 };
-
-
