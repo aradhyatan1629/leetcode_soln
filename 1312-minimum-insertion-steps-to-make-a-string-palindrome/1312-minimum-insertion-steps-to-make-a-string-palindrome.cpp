88f4dd3b704c714eb534d8e2803836ae7ebad080
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2024-04-14 17:24:01 +0530

    Time: 55 ms (12.22%), Space: 29.8 MB (23.89%) - LeetHub

---
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int solve(int ind1,int ind2,string &s1,string &s2,vector<vector<int>> &dp)
+    {
+        if(ind1<0 || ind2<0)
+            return 0;
+        if(dp[ind1][ind2]!=-1)
+            return dp[ind1][ind2];
+        if(s1[ind1] == s2[ind2])
+            return dp[ind1][ind2] = 1 + solve(ind1-1,ind2-1,s1,s2,dp);
+        return dp[ind1][ind2] = max(solve(ind1-1,ind2,s1,s2,dp),solve(ind1,ind2-1,s1,s2,dp));
+    }
+    
+    int minInsertions(string s) {
+        int n = s.size();
+        string s1 = s;
+        reverse(s.begin(),s.end());
+        vector<vector<int>> dp(n,vector<int>(n,-1));
+        int len = solve(n-1,n-1,s1,s,dp);
+        return n-len;
+    }
+};
+
