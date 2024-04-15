1f694d5fde56e3bec8ae0a7d5888e2126c7b1ba2
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2024-04-16 00:19:55 +0530

    Time: 6 ms (89.40%), Space: 14.1 MB (16.97%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,22 +1,13 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int n=nums.size();
         unordered_map<int,int> m;
         for(int i=0;i<nums.size();i++)
         {
-            int x = target-nums[i];
-            if(m.find(x)!=m.end())
-            {
-                return {m[x],i};
-            }
-            m[nums[i]]=i;
+            if(m.find(target-nums[i]) != m.end())
+                return {m[target-nums[i]],i};
+            m[nums[i]] = i;
         }
-        return {};
+        return {-1,-1};
     }
 };
-
-// tc - O(n) on best case 
-/*
-In the worst case(which rarely happens), the unordered_map takes O(N) to find an element. In that case, the time complexity will be O(N2). If we use map instead of unordered_map, the time complexity will be O(N* logN) as the map data structure takes logN time to find an element.
-*/
