8c244148aeda3042c2c391147058ffa60c9cf302
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2024-04-16 00:33:52 +0530

    Time: 61 ms (73.42%), Space: 27.1 MB (94.28%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,37 +1,35 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int n=nums.size();
         vector<vector<int>> ans;
+        sort(nums.begin(),nums.end());
+        int n = nums.size();
         for(int i=0;i<n;i++)
         {
-            if(i>0 and nums[i]==nums[i-1])
+            if(i>0 && nums[i]==nums[i-1])
                 continue;
-            int j=i+1;
-            int k=n-1;
+            int j = i+1;
+            int k = n-1;
             while(j<k)
             {
                 int sum = nums[i]+nums[j]+nums[k];
-                if(sum>0)
-                {
-                    k--;
-                }
-                else if(sum<0)
+                if(sum<0)
                 {
                     j++;
+                }
+                else if(sum>0)
+                {
+                    k--;
                 }
                 else
                 {
                     ans.push_back({nums[i],nums[j],nums[k]});
                     j++,k--;
-                    while(j<k and nums[j]==nums[j-1])j++;
-                    while(j<k and nums[k]==nums[k+1])k--;
+                    while(j<k && nums[j]==nums[j-1])j++;
+                    while(j<k && nums[k]==nums[k+1])k--;
                 }
             }
         }
         return ans;
     }
 };
-// tc - O(n^2) + O(nlogn)
-// sc - O(1)
