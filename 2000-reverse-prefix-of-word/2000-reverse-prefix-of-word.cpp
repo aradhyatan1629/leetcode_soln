dbd5099932eead5f5beff3c9b99218d7df9fa3ea
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2024-05-01 11:22:00 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (26.16%) - LeetHub

---
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+        int i=0,j=0;
+        int n = word.size();
+        while(j<n && word[j]!=ch)
+            j++;
+        if(j>=n)
+            return word;
+        while(i<j)
+        {
+            swap(word[i],word[j]);
+            i++,j--;
+        }
+        return word;
+    }
+};
