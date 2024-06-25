57cf0413c1440aa07765c8a30d7c74e4d62e232f
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2024-06-25 14:04:29 +0530

    Time: 4 ms (35.79%), Space: 10.3 MB (6.12%) - LeetHub

--- 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.cpp
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.cpp
@@ -11,46 +11,48 @@
  */
 class Solution {
 public:
+    void solve(TreeNode *root,vector<int> &pre,int &ind,int n)
+    {
+        if(root==NULL)
+            return;
+        solve(root->left,pre,ind,n);
+        if(ind==0){
+            root->val = pre[n-1];
+        }
+        else{
+            root->val = pre[n-1] - pre[ind-1];
+        }
+        ind++;
+        solve(root->right,pre,ind,n);
+    }
     
     void inorder(TreeNode *root,vector<int> &v)
     {
         if(root==NULL)
-        {
             return;
-        }
         inorder(root->left,v);
         v.push_back(root->val);
         inorder(root->right,v);
     }
     
-    int i=0;
-    void solve(TreeNode *root,vector<int> &v,int n)
-    {
-        if(root==NULL)
-        {
-            return;
-        }
-        solve(root->left,v,n);
-        if(i==0)
-        {
-            root->val = v[n-1];
-        }
-        else 
-        {
-            root->val = v[n-1] - v[i-1];
-        }
-        i++;
-        solve(root->right,v,n);
-    }
-    
     TreeNode* bstToGst(TreeNode* root) {
         vector<int> v;
         inorder(root,v);
-        for(int i=1;i<v.size();i++)
-        {
-            v[i]=v[i]+v[i-1];
-        }
-        solve(root,v,v.size());
+        int n = v.size();
+        vector<int> pre(n,0);
+        pre[0] = v[0];
+        for(int i=1;i<n;i++)
+            pre[i] = pre[i-1] + v[i];
+        
+        int ind = 0;
+        solve(root,pre,ind,n);
         return root;
     }
 };
+
+
+/*
+0 1 2 3 4 5 6 7 8
+0 1 3 6 10 15 21 28 36
+
+*/
