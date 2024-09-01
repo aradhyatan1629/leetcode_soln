f1f78c5238120f385df5857b6850e42c6867cc7b
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2024-09-01 20:43:44 +0530

    Time: 201 ms (18.26%), Space: 142.7 MB (6.82%) - LeetHub

--- 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.cpp
+++ 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.cpp
@@ -1,17 +1,35 @@
 class Solution {
 public:
+    bool check(vector<int> &v,int k)
+    {
+        if(abs(v[0]-v[1])>k || abs(v[0]-v[2])>k || abs(v[1]-v[2])>k)return false;
+        return true;
+        
+    }
+    
     vector<vector<int>> divideArray(vector<int>& nums, int k) {
-        vector<vector<int>> ans;
         sort(nums.begin(),nums.end());
-        int n=nums.size();
-        for(int i=0;i<n-2;i+=3)
+        vector<vector<int>> ans;
+        for(int i=0;i<nums.size()-2;i+=3)
         {
-            if((abs(nums[i]-nums[i+1])>k or abs(nums[i]-nums[i+2])>k))
+            vector<int> v;
+            v.push_back(nums[i]);
+            v.push_back(nums[i+1]);
+            v.push_back(nums[i+2]);
+            if(!check(v,k))
                 return {};
-            ans.push_back({nums[i],nums[i+1],nums[i+2]});
+            ans.push_back(v);
         }
         return ans;
+        
     }
 };
 
 
+/*
+
+1,1,3,3,4,5,7,8,9
+
+
+1,5,7,10,14,15
+*/
