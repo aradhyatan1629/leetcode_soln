cf8ac9c1b62e450dcaa47fbed12961f7cd2884f0
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2024-09-11 01:36:50 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (88.58%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -2,37 +2,27 @@ class Solution {
 private:
     vector<vector<int>> ans;
 public:
-    bool check(vector<int> &v,unordered_set<int> &s){
-        for(int i=0;i<v.size();i++){
-            if(s.find(v[i])!=s.end())
-                return false;
-        }
-        return true;
-    }
-    
-    void solve(int ind,vector<int> &nums,vector<int> &v,int k,int target,unordered_set<int> &s,int sz){
+    void solve(int ind,vector<int> &nums,vector<int> &v,int k,int target,int sz){
         
         if(ind >= nums.size()){
             if(target == 0 && v.size() == sz){
                 ans.push_back(v);
-                for(auto x:v)s.insert(x);
             }
             return;
         }
         
-        if(nums[ind] <= target && k>0 ){
+        if(nums[ind] <= target && k>0){
             v.push_back(nums[ind]);
-            solve(ind+1,nums,v,k-1,target-nums[ind],s,sz);
+            solve(ind+1,nums,v,k-1,target-nums[ind],sz);
             v.pop_back();
         }
-        solve(ind+1,nums,v,k,target,s,sz);
+        solve(ind+1,nums,v,k,target,sz);
     }
     
     vector<vector<int>> combinationSum3(int k, int n) {
         vector<int> nums = {1,2,3,4,5,6,7,8,9};
         vector<int> v;
-        unordered_set<int> s;
-        solve(0,nums,v,k,n,s,k);
+        solve(0,nums,v,k,n,k);
         return ans;
     }
 };
