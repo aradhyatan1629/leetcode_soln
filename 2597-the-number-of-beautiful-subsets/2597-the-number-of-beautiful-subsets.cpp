139893218c6919b2d45943af895354e8158f0d81
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2024-09-13 01:43:13 +0530

    Time: 1181 ms (14.87%), Space: 36.8 MB (92.07%) - LeetHub

--- 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.cpp
+++ 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.cpp
@@ -1,38 +1,32 @@
 class Solution {
 public:
-    bool check(vector<int> &v,int k)
-    {
+    bool check(vector<int> &v,int k){
         int n = v.size();
-        for(int i=0;i<n;i++)
-        {
-            for(int j=i+1;j<n;j++)
-            {
-                if(abs(v[i]-v[j])==k)
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(abs(v[i]-v[j]) == k)
                     return false;
             }
         }
         return true;
     }
     
-    void solve(int ind,vector<int> &v,vector<int> &nums,int k,int &ans)
-    {
-        if(ind<0)
-        {
+    void solve(int ind,vector<int> &nums,vector<int> &v,int k,int &ans){
+        if(ind >= nums.size()){
             if(v.size()>0 && check(v,k))
                 ans++;
             return;
         }
         v.push_back(nums[ind]);
-        solve(ind-1,v,nums,k,ans);
+        solve(ind+1,nums,v,k,ans);
         v.pop_back();
-        solve(ind-1,v,nums,k,ans);
+        solve(ind+1,nums,v,k,ans);
     }
-    
+        
     int beautifulSubsets(vector<int>& nums, int k) {
         int ans = 0;
-        int n = nums.size();
         vector<int> v;
-        solve(n-1,v,nums,k,ans);
+        solve(0,nums,v,k,ans);
         return ans;
     }
 };
