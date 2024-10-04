7426775cccf41e8995b40e0df35b4d43b7faefe2
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2024-10-04 13:32:12 +0530

    Time: 138 ms (21.85%), Space: 79.9 MB (9.61%) - LeetHub

--- 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.cpp
+++ 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.cpp
@@ -3,19 +3,33 @@ public:
     bool canArrange(vector<int>& arr, int k) {
         unordered_map<int,int> m;
         for(int i=0;i<arr.size();i++){
-            int rem = ((arr[i] % k) + k) % k;
+            int rem = ((arr[i]%k)+k)%k; // to handle both -ve and +ve nos.
             m[rem]++;
         }
-        
-        if(m[0]%2 != 0)
+        if(m[0]%2!=0)
             return false;
         
         for(int i=1;i<=k/2;i++){
-             if(m[i] != m[k - i]) 
+            if(m[i] != m[k-i])
                 return false;
         }
         
         return true;
     }
 };
+
+
+
+
+
+/*
+(a%k+b%k)%k = 0, if (a+b)%k = 0
+problem converts to 2 sum problem
+using remainders
+*/
+
+
+
+
+
 
