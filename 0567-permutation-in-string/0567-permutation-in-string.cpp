6c1cc13a0db2c5355e28a8e47b7ec9328c0834d3
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2024-10-05 17:56:46 +0530

    Time: 16 ms (28.47%), Space: 10.7 MB (28.55%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,31 +1,51 @@
 class Solution {
 public:
+    bool check(unordered_map<char,int> &m1,unordered_map<char,int> &m2){
+        if(m1.size() != m2.size())
+            return false;
+        for(auto x:m1){
+            if(m2.find(x.first) == m2.end() || m1[x.first] != m2[x.first])
+                return false;
+        }
+        return true;
+    }
+    
     bool checkInclusion(string s1, string s2) {
-        //Sorting and using sliding window
+        if(s1.size() > s2.size())
+            return false;
+        
+        unordered_map<char,int> m1;
+        unordered_map<char,int> m2;
+        
+        for(int i=0;i<s1.size();i++){
+            m1[s1[i]]++;
+        }
+        
         int i=0,j=0,n=s2.size();
-        sort(s1.begin(),s1.end());
-        int k=s1.size();
-        string t;
-        while(j<n)
-        {
-            t+=s2[j];
-            if(j-i+1<k)
-            {
+        int k = s1.size();
+        
+        while(j<n){
+            m2[s2[j]]++;
+            if(j-i+1 < k){
                 j++;
             }
-            else if(j-i+1==k)
-            {
-                string x=t;
-                sort(x.begin(),x.end());
-                if(s1==x)
-                {
+            else if(j-i+1 == k){
+                if(check(m1,m2))
                     return true;
-                }
-                t.erase(t.begin());
-                i++;
-                j++;
+                m2[s2[i]]--;
+                if(m2[s2[i]] == 0)
+                    m2.erase(s2[i]);
+                i++,j++;
             }
         }
         return false;
     }
 };
+
+
+/*
+
+defd    ed
+        
+
+*/
