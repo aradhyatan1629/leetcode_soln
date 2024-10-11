d00a2e4a26b75f1aaa05e01ba66837159423d12c
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2024-10-11 22:24:24 +0530

    Time: 124 ms (95.53%), Space: 55 MB (93.81%) - LeetHub

--- 1942-the-number-of-the-smallest-unoccupied-chair/1942-the-number-of-the-smallest-unoccupied-chair.cpp
+++ 1942-the-number-of-the-smallest-unoccupied-chair/1942-the-number-of-the-smallest-unoccupied-chair.cpp
@@ -2,42 +2,45 @@ class Solution {
 public:
     int smallestChair(vector<vector<int>>& times, int targetFriend) {
         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-        // {leaving,seatNo}
+        // {leavingTime,seatNo}
         
-        int targetA = times[targetFriend][0];
+        int target = times[targetFriend][0];
         sort(times.begin(),times.end());
-        int currSeat = 0;
         
-        priority_queue<int,vector<int>,greater<int>> emptySeats;
-        
+        int ind = -1;
         for(int i=0;i<times.size();i++){
-            if(times[i][0] < targetA){
-                while(!pq.empty() && pq.top().first <= times[i][0]){ // some people have left
-                    emptySeats.push(pq.top().second);
-                    pq.pop();
-                }
-                if(emptySeats.size() != 0){
-                    pq.push({times[i][1],emptySeats.top()});
-                    emptySeats.pop();
-                }
-                else{
-                    pq.push({times[i][1],currSeat});
-                    currSeat++;
-                }
-            }
-            else if(times[i][0] == targetA){
-                // cout<<"hi "<<pq.size()<<endl;
-                while(!pq.empty() && pq.top().first <= times[i][0]){ // some people have left
-                    emptySeats.push(pq.top().second);
-                    pq.pop();
-                }
-                // cout<<"hi "<<pq.size()<<endl;
-                if(emptySeats.size() != 0){
-                   return emptySeats.top();
-                }
-                return currSeat;
+            if(times[i][0] == target){
+                ind = i;
+                break;
             }
         }
-        return -1;
+            
+        priority_queue<int,vector<int>,greater<int>> emptySeats;
+        int currSeat = 0;
+        
+        for(int i=0;i<ind;i++){
+            int newArrive = times[i][0];
+            while(!pq.empty() && pq.top().first <= newArrive){
+                emptySeats.push(pq.top().second);
+                pq.pop();
+            }
+            if(emptySeats.size() != 0){
+                pq.push({times[i][1],emptySeats.top()});
+                emptySeats.pop();
+            }
+            else{
+                pq.push({times[i][1],currSeat});
+                currSeat++;
+            }
+        }
+        
+        while(!pq.empty() && pq.top().first <= target){
+            emptySeats.push(pq.top().second);
+            pq.pop();
+        }
+        if(emptySeats.size() != 0)
+            return emptySeats.top();
+        
+        return currSeat;
     }
 };
