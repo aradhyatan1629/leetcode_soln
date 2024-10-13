5156eb9f182b251b40ae00c29336aebca7737f09
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2024-10-13 14:06:31 +0530

    Time: 205 ms (50.00%), Space: 264 MB (50.00%) - LeetHub

--- 3319-k-th-largest-perfect-subtree-size-in-binary-tree/3319-k-th-largest-perfect-subtree-size-in-binary-tree.cpp
+++ 3319-k-th-largest-perfect-subtree-size-in-binary-tree/3319-k-th-largest-perfect-subtree-size-in-binary-tree.cpp
@@ -11,59 +11,57 @@
  */
 class Solution {
 public:
-    
-    void getSize(TreeNode *root,int &sz){
-        if(root == NULL)
-            return;
-        sz++;
-        getSize(root->left,sz);
-        getSize(root->right,sz);
-    }
-    
-    int height(TreeNode *root){
-        if(root == NULL)
-            return 0;
-        int leftH = height(root->left);
-        int rightH = height(root->right);
-        return 1 + max(leftH,rightH);
-    }
-    
-    bool isPerfect(TreeNode *root,int h,int level){
-        if(root == NULL)
-            return true;
+    bool check(TreeNode *root,int &subSize){
+        int lvl = 1;
+        queue<TreeNode*> q;
+        q.push(root);
+        unordered_set<int> s;
         
-        if(root->left == NULL && root->right == NULL){
-            return h == level;
+        while(!q.empty()){
+            int sz = q.size();
+            subSize += q.size();
+            for(int i=0;i<sz;i++){
+                TreeNode *node = q.front();
+                q.pop();
+                if(node->left == NULL && node->right == NULL){
+                    if(s.size() == 0)
+                        s.insert(lvl);
+                    else if(lvl != *s.begin())
+                        return false;
+                }
+                else if(node->left == NULL || node->right == NULL)
+                    return false;
+                if(node->left != NULL)
+                    q.push(node->left);
+                if(node->right != NULL)
+                    q.push(node->right);
+            }
+            lvl++;
         }
-        
-        if(root->left == NULL || root->right == NULL){
-            return false;
-        }
-        
-        bool l = isPerfect(root->left,h,level+1);
-        bool r = isPerfect(root->right,h,level+1);
-        return l && r;
-    }
-    
-    void solve(TreeNode *root,vector<int> &v){
-        if(root == NULL)
-            return;
-        int h = height(root);
-        h--;
-        // cout<<"root "<<root->val<<" height "<<h<<endl;
-        if(isPerfect(root,h,0)){
-            // cout<<" yes "<<root->val<<endl;
-            int sz = 0;
-            getSize(root,sz);
-            v.push_back(sz);
-        }
-        solve(root->left,v);
-        solve(root->right,v);
+        return s.size() == 1;
     }
     
     int kthLargestPerfectSubtree(TreeNode* root, int k) {
+        queue<TreeNode*> q;
+        q.push(root);
         vector<int> v;
-        solve(root,v);
+        
+        while(!q.empty()){
+            int sz = q.size();
+            for(int i=0;i<sz;i++){
+                TreeNode *node = q.front();
+                q.pop();
+                int sz = 0;
+                if(check(node,sz)){
+                    v.push_back(sz);
+                }
+                if(node->left)
+                    q.push(node->left);
+                if(node->right)
+                    q.push(node->right);
+            }
+        }
+        
         sort(v.begin(),v.end(),greater<int>());
         if(k > v.size())
             return -1;
