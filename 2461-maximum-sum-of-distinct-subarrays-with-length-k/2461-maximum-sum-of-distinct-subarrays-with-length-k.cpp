10c128fe4e09bf9fef0bd59d467f52a3d5b3a630
aradhyatan1629 <109618602+aradhyatan1629@users.noreply.github.com>
2024-11-20 01:58:12 +0530

    Time: 119 ms (42.77%), Space: 94.9 MB (94.33%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -1,47 +1,27 @@
 class Solution {
 public:
     long long maximumSubarraySum(vector<int>& nums, int k) {
-        long long sum=0;
-        long long mx=0;
-        int i=0,j=0,n=nums.size();
         unordered_map<int,int> m;
-        while(j<n)
-        {
+        int i=0,j=0,n=nums.size();
+        long long ans = 0;
+        long long sum = 0;
+        
+        while(j<n){
             m[nums[j]]++;
-            sum+=nums[j];
-            if(j-i+1<k)
-            {
+            sum += nums[j];
+            if(j-i+1 < k){
                 j++;
             }
-            else if(j-i+1==k)
-            {
-                if(m.size()==k)
-                {
-                    mx=max(mx,sum);
-                    sum-=nums[i];
-                    // m.erase(nums[i]);
-                    m[nums[i]]--;
-                    if(m[nums[i]]==0)
-                    {
-                        m.erase(nums[i]);
-                    }
-                    i++;
-                    j++;
+            else if(j-i+1 == k){
+                if(m.size() == k){
+                    ans = max(ans,sum);
                 }
-                else
-                {
-                    m[nums[i]]--;
-                    sum-=nums[i];
-                    if(m[nums[i]]==0)
-                    {
-                        m.erase(nums[i]);
-                    }
-                    i++;
-                    j++;
-                }
+                m[nums[i]]--;
+                if(m[nums[i]] == 0)m.erase(nums[i]);
+                sum -= nums[i];
+                i++,j++;
             }
         }
-        return mx;
-//         1,5,5,4,2,9,9,9
+        return ans;
     }
 };
